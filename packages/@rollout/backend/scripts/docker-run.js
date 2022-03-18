0947ae86eb1af6b638d6b4bd7c6867f803f47e27
mfrachet <marvin.frachet@strapi.io>
2022-03-18 07:24:21 +0100

    fix(docker): making docker-compose startup correctly

--- packages/@rollout/backend/scripts/docker-run.js
+++ packages/@rollout/backend/scripts/docker-run.js
@@ -4,8 +4,11 @@ const database = (process.env.DATABASE || 'sqlite').toLocaleLowerCase();
 
 function dockerRun() {
   console.log(`Running on ${database.toUpperCase()}`);
+
   execSync(`npm run db:gen:${database}-schema`, { stdio: 'inherit' });
-  execSync('npm run db:migrate:dev && npm start', { stdio: 'inherit' });
+  execSync('npm run db:migrate:dev', { stdio: 'inherit' });
+  execSync('npm run db:generate:schema', { stdio: 'inherit' });
+  execSync('npm start', { stdio: 'inherit' });
 }
 
 dockerRun();
