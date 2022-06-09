62fca38ee61abc8723fda5a36460253c67368eba
mfrachet <marvin.frachet@strapi.io>
2022-06-09 14:35:23 +0200

    styles(avatar): avatar + empty insights

--- packages/frontend/cypress/e2e/projects/id/environments/envId/flags/flagId/insights.spec.ts
+++ packages/frontend/cypress/e2e/projects/id/environments/envId/flags/flagId/insights.spec.ts
@@ -29,11 +29,12 @@ describe("/dashboard/projects/[id]/environments/[envId]/flags/[flagId]/insights"
     describe("user: Marvin", () => {
       beforeEach(() => {
         cy.signIn("Marvin");
-        cy.visit("/dashboard/projects/1/environments/1/flags/1/insights");
-        cy.injectAxe();
       });
 
-      it("shows the layout", () => {
+      it("shows the layout (with data)", () => {
+        cy.visit("/dashboard/projects/1/environments/1/flags/1/insights");
+        cy.injectAxe();
+
         cy.title().should(
           "eq",
           "Progressively | Project from seeding | Production | New homepage | Insights"
@@ -75,6 +76,51 @@ describe("/dashboard/projects/[id]/environments/[envId]/flags/[flagId]/insights"
         cy.findByRole("heading", { name: "New homepage" }).should("be.visible");
         cy.findByRole("heading", { name: "Insights" }).should("be.visible");
         cy.findByText("Number of hits per date").should("be.visible");
+
+        cy.checkA11y();
+      });
+
+      it("shows the layout (without data)", () => {
+        cy.visit("/dashboard/projects/1/environments/1/flags/2/insights");
+        cy.injectAxe();
+
+        cy.title().should(
+          "eq",
+          "Progressively | Project from seeding | Production | New footer | Insights"
+        );
+
+        cy.findByRole("link", { name: "Projects" })
+          .should("be.visible")
+          .and("have.attr", "href", "/dashboard");
+
+        cy.findByRole("link", { name: "Project from seeding" })
+          .should("be.visible")
+          .and("have.attr", "href", "/dashboard/projects/1");
+
+        cy.findByRole("link", { name: "Production" })
+          .should("be.visible")
+          .and(
+            "have.attr",
+            "href",
+            "/dashboard/projects/1/environments/1/flags"
+          );
+
+        cy.findByRole("link", { name: "Insights" })
+          .should("be.visible")
+          .and(
+            "have.attr",
+            "href",
+            "/dashboard/projects/1/environments/1/flags/2/insights"
+          )
+          .and("have.attr", "aria-current", "page");
+
+        cy.findByRole("heading", { name: "New footer" }).should("be.visible");
+        cy.findByRole("heading", { name: "Insights" }).should("be.visible");
+
+        cy.findByText("No hits found").should("be.visible");
+        cy.findByText(
+          "There are no hits for this flag. Make sure to activate the flag in order to collect hits."
+        ).should("be.visible");
 
         cy.checkA11y();
       });
