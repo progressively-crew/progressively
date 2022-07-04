e8a9cbab3c16a718ee8f310513d62a15eded5639
mfrachet <marvin.frachet@strapi.io>
2022-07-04 09:24:44 +0200

    feat(ab): add empty state for experiment variations

---
+++ packages/frontend/app/modules/ab/components/AbNavBar.tsx
@@ -0,0 +1,31 @@
+import { AiOutlineSetting } from "react-icons/ai";
+import { HorizontalNav, NavItem } from "~/components/HorizontalNav";
+import { IoShapes } from "react-icons/io5";
+
+export interface AbNavBarProps {
+  projectId: string;
+  envId: string;
+  experimentId: string;
+}
+
+export const AbNavBar = ({ projectId, envId, experimentId }: AbNavBarProps) => {
+  const rootEnvPath = `/dashboard/projects/${projectId}/environments/${envId}/ab`;
+
+  return (
+    <HorizontalNav label={`A/B experiment related`}>
+      <NavItem
+        to={`${rootEnvPath}/${experimentId}/variations`}
+        icon={<IoShapes />}
+      >
+        Variations
+      </NavItem>
+
+      <NavItem
+        to={`${rootEnvPath}/${experimentId}/settings`}
+        icon={<AiOutlineSetting />}
+      >
+        Settings
+      </NavItem>
+    </HorizontalNav>
+  );
+};
