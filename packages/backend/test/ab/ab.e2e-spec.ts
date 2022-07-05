a2722ae1f48cfbda76c971e4be75ff918af4030e
mfrachet <marvin.frachet@strapi.io>
2022-07-05 06:48:59 +0200

    feat(ab): delete an experiment and cascade

--- packages/backend/test/ab/ab.e2e-spec.ts
+++ packages/backend/test/ab/ab.e2e-spec.ts
@@ -191,4 +191,76 @@ describe('AbController (e2e)', () => {
         });
     });
   });
+
+  describe('/experiments/1 (DELETE)', () => {
+    it('gives a 401 when the user is not authenticated', () =>
+      verifyAuthGuard(app, '/experiments/1', 'delete'));
+
+    it('gives a 403 when trying to access a valid project but an invalid env', async () => {
+      const access_token = await authenticate(app);
+
+      return request(app.getHttpServer())
+        .delete('/experiments/2')
+        .set('Authorization', `Bearer ${access_token}`)
+        .expect(403)
+        .expect({
+          statusCode: 403,
+          message: 'Forbidden resource',
+          error: 'Forbidden',
+        });
+    });
+
+    it('gives a 403 when the user requests a forbidden project', async () => {
+      const access_token = await authenticate(
+        app,
+        'jane.doe@gmail.com',
+        'password',
+      );
+
+      return request(app.getHttpServer())
+        .delete('/experiments/1')
+        .set('Authorization', `Bearer ${access_token}`)
+        .expect(403)
+        .expect({
+          statusCode: 403,
+          message: 'Forbidden resource',
+          error: 'Forbidden',
+        });
+    });
+
+    it('gives a 403 when the user is not allowed to perform the action', async () => {
+      const access_token = await authenticate(
+        app,
+        'john.doe@gmail.com',
+        'password',
+      );
+
+      return request(app.getHttpServer())
+        .delete('/experiments/1')
+        .set('Authorization', `Bearer ${access_token}`)
+        .expect(403)
+        .expect({
+          statusCode: 403,
+          message: 'Forbidden resource',
+          error: 'Forbidden',
+        });
+    });
+
+    it('gives a 200 when the user is allowed to perform the action', async () => {
+      const access_token = await authenticate(app);
+
+      const response = await request(app.getHttpServer())
+        .delete('/experiments/1')
+        .set('Authorization', `Bearer ${access_token}`);
+
+      expect(response.status).toBe(200);
+
+      // Make sure the user can't access the project anymore
+      const getResponse = await request(app.getHttpServer())
+        .get('/experiments/1')
+        .set('Authorization', `Bearer ${access_token}`);
+
+      expect(getResponse.status).toBe(403);
+    });
+  });
 });
