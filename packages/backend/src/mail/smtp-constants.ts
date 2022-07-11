c5b6be7e6e52127b681b6540945369a4a5997ba7
mfrachet <marvin.frachet@strapi.io>
2022-07-11 19:41:20 +0200

    adjusting .env

--- packages/backend/src/mail/smtp-constants.ts
+++ packages/backend/src/mail/smtp-constants.ts
@@ -1,7 +1,6 @@
 export const Smtp = {
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: process.env.SMTP_SECURE,
   user: process.env.SMTP_USER,
   password: process.env.SMTP_PASSWORD,
 };
