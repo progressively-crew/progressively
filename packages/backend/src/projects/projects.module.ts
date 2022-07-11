f67b3ccb12e3aba836f6c2e1e847935be9b29a3b
mfrachet <marvin.frachet@strapi.io>
2022-07-11 07:19:03 +0200

    database module

--- packages/backend/src/projects/projects.module.ts
+++ packages/backend/src/projects/projects.module.ts
@@ -1,22 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
-import { FlagsModule } from '../flags/flags.module';
-import { StrategyModule } from '../strategy/strategy.module';
 import { EnvironmentsModule } from '../environments/environments.module';
 import { UsersModule } from '../users/users.module';
 import { MailModule } from '../mail/mail.module';
 import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  imports: [
-    FlagsModule,
-    StrategyModule,
-    EnvironmentsModule,
-    UsersModule,
-    MailModule,
-    DatabaseModule,
-  ],
+  imports: [EnvironmentsModule, UsersModule, MailModule, DatabaseModule],
   controllers: [ProjectsController],
   providers: [ProjectsService],
   exports: [ProjectsService],
