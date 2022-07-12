635f0f9c9e0c4f00e8046b9ca60df71379e05f0d
mfrachet <marvin.frachet@strapi.io>
2022-07-12 09:52:43 +0200

    fix(error): improve error pages styles

--- packages/frontend/app/layouts/ErrorLayout.tsx
+++ packages/frontend/app/layouts/ErrorLayout.tsx
@@ -1,13 +1,21 @@
 import { Container } from "~/components/Container";
 import { Main } from "~/components/Main";
+import { styled } from "~/stitches.config";
 
 export interface ErrorLayoutProps {
   children: React.ReactNode;
 }
+
+const Wrapper = styled("div", {
+  padding: "$spacing$10 0",
+});
+
 export function ErrorLayout({ children }: ErrorLayoutProps) {
   return (
-    <Container>
-      <Main>{children}</Main>
-    </Container>
+    <Wrapper>
+      <Container>
+        <Main>{children}</Main>
+      </Container>
+    </Wrapper>
   );
 }
--- packages/frontend/app/routes/404.tsx
+++ packages/frontend/app/routes/404.tsx
@@ -1,5 +1,6 @@
 import { Button } from "~/components/Buttons/Button";
 import { H1 } from "~/components/H1";
+import { Spacer } from "~/components/Spacer";
 import { Typography } from "~/components/Typography";
 import { ErrorLayout } from "~/layouts/ErrorLayout";
 
@@ -9,6 +10,8 @@ export default function NotFoundPage() {
       <H1>{`Woops! This page does not exist.`}</H1>
 
       <Typography>{`It looks you're trying to access a content that does not exist.`}</Typography>
+
+      <Spacer size={4} />
 
       <Button to="/signin">Signin page</Button>
     </ErrorLayout>
