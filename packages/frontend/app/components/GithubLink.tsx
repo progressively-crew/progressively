33f0c219b77c0e0bf7bce12671bcae7179c766bb
mfrachet <marvin.frachet@strapi.io>
2022-07-12 16:22:43 +0200

    fix

--- packages/frontend/app/components/GithubLink.tsx
+++ packages/frontend/app/components/GithubLink.tsx
@@ -5,8 +5,25 @@ const GithubLinkWrapper = styled("a", {
   fontSize: "$content",
   color: "$content",
   fontFamily: "$default",
-  height: "$cta",
   alignItems: "center",
+  background: "$background",
+  borderRadius: "$borderRadius$regular",
+  padding: "0 $spacing$4",
+  height: "$cta",
+  cursor: "pointer",
+  textDecoration: "none",
+  border: "1px dashed transparent",
+  transition: "all 0.1s",
+
+  "&:hover": {
+    color: "$title",
+    transform: "scale(1.05)",
+  },
+
+  "&:active": {
+    color: "$title",
+    border: "1px dashed $hover",
+  },
 });
 
 export const GithubLink = () => {
