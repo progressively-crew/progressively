87fba0f9221131f73b8913e2da5f66a10aa57682
mfrachet <marvin.frachet@strapi.io>
2022-07-15 09:16:13 +0200

    fix(flags): forward the cookie id to the API

--- example/nextjs/pages/index.tsx
+++ example/nextjs/pages/index.tsx
@@ -46,6 +46,10 @@ export async function getServerSideProps({
   const { ssrProps, cookies } = await getSSRProps("valid-sdk-key", {
     websocketUrl: "ws://localhost:4000",
     apiUrl: "http://localhost:4000",
+    fields: {
+      email: "marvin.frachet@something.com",
+      id: "1",
+    },
   });
 
   res.setHeader("set-cookie", cookies);
