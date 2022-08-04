094df2c1fb54e51b799750fbbd7926080396f0e2
mfrachet <marvin.frachet@strapi.io>
2022-08-04 09:43:37 +0200

    feat(storybook): add box stories

--- packages/frontend/app/components/stories/Boxes/WarningBox.stories.tsx
+++ packages/frontend/app/components/stories/Boxes/WarningBox.stories.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Link } from "../Link";
+import { WarningBox } from "../../WarningBox";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Components/Link",
-  component: Link,
+  title: "Components/Boxes/WarningBox",
+  component: WarningBox,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: "color" },
   },
-} as ComponentMeta<typeof Link>;
+} as ComponentMeta<typeof WarningBox>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const DefaultTemplate: ComponentStory<typeof Link> = (args) => (
-  <Link {...args} />
+const DefaultTemplate: ComponentStory<typeof WarningBox> = (args) => (
+  <WarningBox {...args} />
 );
 export const Default = DefaultTemplate.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Default.args = {
-  to: "/",
-  children: "Navigate",
+  title: "Be ready for something dangereous",
+  list: { warning: "Some warning", another: "Another one" },
 };
