0ee7804f38a3379f5b341486b6ef6741e29ed87b
mfrachet <marvin.frachet@strapi.io>
2022-08-08 12:00:30 +0200

    wip

--- packages/frontend/app/components/Spinner.tsx
+++ packages/frontend/app/components/Spinner.tsx
@@ -10,6 +10,6 @@ export const Spinner = styled("div", {
   height: "10px",
   border: "3px solid $apollo",
   borderRadius: " 50%",
-  borderTopColor: "#fff",
+  borderTopColor: "$currentColor",
   animation: `${spin} 1s ease-in-out infinite`,
 });
