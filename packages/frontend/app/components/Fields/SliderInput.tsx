49ec67a91fe01780ac0a629f129d0308cc99a26d
mfrachet <marvin.frachet@strapi.io>
2022-08-11 09:21:19 +0200

    wip

--- packages/frontend/app/components/Fields/SliderInput.tsx
+++ packages/frontend/app/components/Fields/SliderInput.tsx
@@ -9,6 +9,43 @@ export interface SliderInputProps {
   onChange: (nextValue: number) => void;
 }
 
+const RangeInput = styled("input", {
+  "-webkit-appearance": "none",
+  appearance: "none",
+  background: "transparent",
+  cursor: "pointer",
+  width: "15rem",
+
+  // chrome edge safari
+  "&::-webkit-slider-runnable-track": {
+    background: "#053a5f",
+    height: "0.5rem",
+  },
+
+  "&::-webkit-slider-thumb": {
+    "-webkit-appearance": "none",
+    appearance: "none",
+    marginTop: "-12px",
+    backgroundColor: " #5cd5eb",
+    height: "2rem",
+    width: "1rem",
+  },
+
+  // firefox
+  "&::-moz-range-track": {
+    background: "#053a5f",
+    height: "0.5rem",
+  },
+
+  "&::-moz-range-thumb": {
+    border: "none",
+    borderRadius: 0,
+    backgroundColor: "#5cd5eb",
+    height: "2rem",
+    width: "1rem",
+  },
+});
+
 export const SliderInput = ({
   percentageValue,
   name,
@@ -19,7 +56,7 @@ export const SliderInput = ({
     <Stack spacing={2}>
       <Label htmlFor={`field-${name}`}>{label}</Label>
 
-      <input
+      <RangeInput
         type="range"
         min={0}
         max={100}
