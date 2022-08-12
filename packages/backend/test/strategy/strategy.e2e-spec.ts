e9ab47c5aa934b9e4eca83d22b12c5cc0fb671f4
mfrachet <marvin.frachet@strapi.io>
2022-08-12 15:11:40 +0200

    backend

--- packages/backend/test/strategy/strategy.e2e-spec.ts
+++ packages/backend/test/strategy/strategy.e2e-spec.ts
@@ -168,4 +168,137 @@ describe('Strategy (e2e)', () => {
       `);
     });
   });
+
+  describe('/strategies/1 (EDIT)', () => {
+    it('gives a 401 when the user is not authenticated', () =>
+      verifyAuthGuard(app, '/strategies/1', 'put'));
+
+    it('gives a 403 when trying to access a valid project but an invalid env', async () => {
+      const access_token = await authenticate(app);
+
+      return request(app.getHttpServer())
+        .put('/strategies/3')
+        .set('Authorization', `Bearer ${access_token}`)
+        .expect(403)
+        .expect({
+          statusCode: 403,
+          message: 'Forbidden resource',
+          error: 'Forbidden',
+        });
+    });
+
+    it('gives a 403 when the user requests a forbidden project', async () => {
+      const access_token = await authenticate(
+        app,
+        'jane.doe@gmail.com',
+        'password',
+      );
+
+      return request(app.getHttpServer())
+        .put('/strategies/1')
+        .set('Authorization', `Bearer ${access_token}`)
+        .expect(403)
+        .expect({
+          statusCode: 403,
+          message: 'Forbidden resource',
+          error: 'Forbidden',
+        });
+    });
+
+    it('gives 400 when the project has noname', async () => {
+      const access_token = await authenticate(app);
+
+      const invalidStrategy: any = {
+        name: undefined,
+        strategyRuleType: 'default',
+        rolloutPercentage: 100,
+      };
+
+      await request(app.getHttpServer())
+        .put('/strategies/1')
+        .set('Authorization', `Bearer ${access_token}`)
+        .send(invalidStrategy)
+        .expect(400)
+        .expect({
+          statusCode: 400,
+          message: 'Validation failed',
+          error: 'Bad Request',
+        });
+    });
+
+    it('gives 400 when the project receives a wrong strategy type', async () => {
+      const access_token = await authenticate(app);
+
+      const invalidStrategy: any = {
+        name: 'Super strategy',
+        strategyRuleType: 'invalid strategy',
+        rolloutPercentage: 100,
+      };
+
+      await request(app.getHttpServer())
+        .put('/strategies/1')
+        .set('Authorization', `Bearer ${access_token}`)
+        .send(invalidStrategy)
+        .expect(400)
+        .expect({
+          statusCode: 400,
+          message: 'Validation failed',
+          error: 'Bad Request',
+        });
+    });
+
+    ['fieldName', 'fieldComparator', 'fieldValue'].forEach((field) => {
+      it(`gives 400 when the project has a strategy of type "field" but no "${field}"`, async () => {
+        const access_token = await authenticate(app);
+
+        const invalidStrategy: any = {
+          name: 'Super strategy',
+          strategyRuleType: 'field',
+          rolloutPercentage: 100,
+          [field]: undefined,
+        };
+
+        await request(app.getHttpServer())
+          .put('/strategies/1')
+          .set('Authorization', `Bearer ${access_token}`)
+          .send(invalidStrategy)
+          .expect(400)
+          .expect({
+            statusCode: 400,
+            message: 'Validation failed',
+            error: 'Bad Request',
+          });
+      });
+    });
+
+    it('updates the strategy strategy', async () => {
+      const access_token = await authenticate(app);
+
+      const validStrategy: any = {
+        name: 'Super strategy 2',
+        strategyRuleType: 'default',
+        rolloutPercentage: 67,
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/environments/1/flags/1/strategies')
+        .set('Authorization', `Bearer ${access_token}`)
+        .send(validStrategy)
+        .expect(201);
+
+      const { uuid, ...obj } = response.body;
+
+      expect(uuid).toBeDefined();
+      expect(obj).toEqual({
+        name: 'Super strategy 2',
+        strategyRuleType: 'default',
+        fieldName: null,
+        fieldComparator: null,
+        fieldValue: null,
+        rolloutPercentage: 67,
+        flagEnvironmentFlagId: '1',
+        flagEnvironmentEnvironmentId: '1',
+      });
+    });
+  });
 });
