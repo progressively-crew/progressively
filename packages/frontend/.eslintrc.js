751d17d9142b67125f7267a992ee436761eff5bd
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2022-08-21 14:54:23 +0200

    disable filename-case and prefere module rules

--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -27,6 +27,8 @@ module.exports = {
     "react/prop-types": "off",
     "sonarjs/no-duplicate-string": "off",
     "react/no-unescaped-entities": "off",
+    "unicorn/filename-case": "off",
+    "unicorn/prefer-module": "off",
     "unicorn/prevent-abbreviations": "off",
   },
 };
