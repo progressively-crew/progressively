8891e166e818fdd70c9ea86d45a20d090e9ce920
mfrachet <marvin.frachet@openclassrooms.com>
2022-09-06 08:58:26 +0200

    add tests for simple variant

--- packages/frontend/app/modules/strategies/components/__tests__/StrategyDescription.spec.tsx
+++ packages/frontend/app/modules/strategies/components/__tests__/StrategyDescription.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { expect } from "@jest/globals";
 import { FlagEnv, FlagStatus } from "~/modules/flags/types";
 import { StrategyDescription } from "../StrategyDescription";
@@ -33,8 +33,61 @@ describe("StrategyDescription", () => {
   });
 
   it("shows a generic message when the flag is not activated", () => {
+    flagEnv.status = FlagStatus.NOT_ACTIVATED;
     render(<StrategyDescription flagEnv={flagEnv} hasStrategies={false} />);
 
-    expect(getText()).toBe('12% of the audience will resolve the "true" variant of the flag.');
+    expect(getText()).toBe(`Nobody will receive the "true" variant of the flag: it'snot activated`);
+  });
+
+  describe("No variants", () => {
+    beforeEach(() => {
+      flagEnv.variants = [];
+    });
+
+    it("shows a specific message when the percentage is 12% with no strategies", () => {
+      const hasStrategies = false;
+
+      render(<StrategyDescription flagEnv={flagEnv} hasStrategies={hasStrategies} />);
+
+      expect(getText()).toBe('12% of the audience will resolve the "true" variant of the flag.');
+    });
+
+    it("shows a specific message when the percentage is 12% with strategies", () => {
+      const hasStrategies = true;
+
+      render(<StrategyDescription flagEnv={flagEnv} hasStrategies={hasStrategies} />);
+
+      expect(getText()).toBe(
+        `12% of the audience AND the users matching at least one of the following strategies will resolve the "true" variant of the flag.`
+      );
+    });
+
+    it("shows a specific message when the percentage is 100%", () => {
+      flagEnv.rolloutPercentage = 100;
+
+      render(<StrategyDescription flagEnv={flagEnv} hasStrategies={false} />);
+
+      expect(getText()).toBe(`100% of the audience will resolve the "true" variant of the flag.`);
+    });
+
+    it("shows a specific message when the percentage is 0% and with no strategies", () => {
+      flagEnv.rolloutPercentage = 0;
+      const hasStrategies = false;
+
+      render(<StrategyDescription flagEnv={flagEnv} hasStrategies={hasStrategies} />);
+
+      expect(getText()).toBe(`0% of the audience will resolve the "true" variant of the flag.`);
+    });
+
+    it("shows a specific message when the percentage is 0% and with strategies", () => {
+      flagEnv.rolloutPercentage = 0;
+      const hasStrategies = true;
+
+      render(<StrategyDescription flagEnv={flagEnv} hasStrategies={hasStrategies} />);
+
+      expect(getText()).toBe(
+        `Only the user matching at least one of the following strategies will resolve the "true" variant of the flag since the rollout percentage is 0%.`
+      );
+    });
   });
 });
