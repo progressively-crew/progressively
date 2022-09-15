6959c631700f9cfbccae4af272c915db8e03a0a6
mfrachet <marvin.frachet@openclassrooms.com>
2022-09-15 11:22:05 +0200

    good stuff, I think

--- packages/frontend/app/modules/misc/components/TreeToggle.tsx
+++ packages/frontend/app/modules/misc/components/TreeToggle.tsx
@@ -1,4 +1,4 @@
-import { FaTree } from "react-icons/fa";
+import { TbSailboat } from "react-icons/tb";
 import { useNavToggle } from "~/components/Breadcrumbs/hooks/useNavToggle";
 import { Button } from "~/components/Buttons/Button";
 
@@ -6,8 +6,8 @@ export const TreeToggle = () => {
   const { toggleNav } = useNavToggle();
 
   return (
-    <Button onClick={toggleNav} icon={<FaTree />} variant="tertiary" scheme="inverse">
-      Browse
+    <Button onClick={toggleNav} icon={<TbSailboat />} variant="tertiary" scheme="inverse">
+      Navigate
     </Button>
   );
 };
