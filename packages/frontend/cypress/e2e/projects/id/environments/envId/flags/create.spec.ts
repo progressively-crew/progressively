02c9d79d6bd92c5d029e3afe084b3a748fd98545
mfrachet <marvin.frachet@strapi.io>
2022-09-20 10:06:29 +0200

    remove unecessary async / await

--- packages/frontend/cypress/e2e/projects/id/environments/envId/flags/create.spec.ts
+++ packages/frontend/cypress/e2e/projects/id/environments/envId/flags/create.spec.ts
@@ -84,7 +84,7 @@ describe("/dashboard/projects/[id]/environments/[envId]/flags/create", () => {
         cy.checkA11y();
       });
 
-      it.only("creates a flag ", () => {
+      it("creates a flag ", () => {
         cy.findByLabelText("Flag name").type("My new flag");
         cy.findByLabelText("Flag description").type("My new flag description");
         cy.findByRole("button", { name: "Create the feature flag" }).click();
