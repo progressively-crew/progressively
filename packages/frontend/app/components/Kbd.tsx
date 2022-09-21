11a8d0a868f921b634d5816c833e58bcc7528d0d
mfrachet <marvin.frachet@strapi.io>
2022-09-21 07:56:12 +0200

    better keyboard

--- packages/frontend/app/components/Kbd.tsx
+++ packages/frontend/app/components/Kbd.tsx
@@ -1,9 +1,9 @@
 import { styled } from "~/stitches.config";
 
 export const Kbd = styled("kbd", {
-  backgroundColor: "$heracles",
+  backgroundColor: "$hera",
   borderRadius: "$borderRadius$small",
-  border: "1px solid $hadesLight",
+  border: "1px solid $heracles",
   boxShadow: "inset 0 0 8px #bbb, 0 1px 0 #aaa, 0 4px 0 #bbb",
   color: "$hades",
   display: "inline-flex",
@@ -12,4 +12,17 @@ export const Kbd = styled("kbd", {
   fontSize: "$neptune",
   width: "$keyboardKey",
   height: "$keyboardKey",
+  transition: "all 0.2s",
+
+  variants: {
+    isPressed: {
+      true: {
+        boxShadow: "inset 0 0 25px #ddd, 0 0 3px #333,  0 2px 0 #bbb",
+        color: "#888",
+        background: "#ebeced",
+        marginTop: "7px",
+        borderTop: "1px solid #eee",
+      },
+    },
+  },
 });
--- packages/frontend/app/modules/misc/components/TreeToggle.tsx
+++ packages/frontend/app/modules/misc/components/TreeToggle.tsx
@@ -1,16 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavToggle } from "~/components/Breadcrumbs/hooks/useNavToggle";
 import { Button } from "~/components/Buttons/Button";
 import { HStack } from "~/components/HStack";
 import { Kbd } from "~/components/Kbd";
 
 export const TreeToggle = () => {
-  const { toggleNav } = useNavToggle();
+  const { toggleNav, isNavOpened } = useNavToggle();
+  const [isPressed, setIsPressed] = useState(false);
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       if (e.key === "k" && e.metaKey) {
         e.preventDefault();
+        setIsPressed(true);
         toggleNav();
       }
     };
@@ -22,13 +24,19 @@ export const TreeToggle = () => {
     };
   }, [toggleNav]);
 
+  useEffect(() => {
+    if (!isNavOpened) {
+      setIsPressed(false);
+    }
+  }, [isNavOpened]);
+
   return (
     <Button
       onClick={toggleNav}
       icon={
         <HStack spacing={1}>
-          <Kbd>⌘</Kbd>
-          <Kbd>K</Kbd>
+          <Kbd isPressed={isPressed}>⌘</Kbd>
+          <Kbd isPressed={isPressed}>K</Kbd>
         </HStack>
       }
       variant="tertiary"
