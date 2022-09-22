e6eea629d3573387dfae23d0995125ba05a338b1
mfrachet <marvin.frachet@strapi.io>
2022-09-22 16:59:43 +0200

    wip

--- packages/frontend/jest.config.js
+++ packages/frontend/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: "ts-jest",
   rootDir: "app",
   moduleNameMapper: {
+    "~/components/(.*)$": "<rootDir>/../../ui-components/src/$1",
     "~/(.*)$": "<rootDir>/$1",
   },
   testEnvironment: "jsdom",
