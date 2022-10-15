117501e3d3862a746713aa0ea4345c58bf91cdc6
mfrachet <marvin.frachet@strapi.io>
2022-10-15 22:50:40 +0200

    wip

--- packages/documentation/docs/.vitepress/config.js
+++ packages/documentation/docs/.vitepress/config.js
@@ -17,6 +17,7 @@ export default {
         items: [
           { text: `Why Progressively?`, link: "/introduction/why" },
           { text: "Getting Started", link: "/introduction/getting-started" },
+          { text: "Configuration", link: "/introduction/configuration" },
         ],
       },
       {
