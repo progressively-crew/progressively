baeec572f9a920dd99f786bf7c0ab756998aeaec
Marvin Frachet <marvin.frachet@protonmail.com>
2022-10-18 22:04:10 +0200

    refacto(flags): add explicit section left side (#241)

--- packages/frontend/cypress/e2e/projects/id/environments/envId/flags/flagId/index.spec.ts
+++ packages/frontend/cypress/e2e/projects/id/environments/envId/flags/flagId/index.spec.ts
@@ -47,9 +47,7 @@ describe("/dashboard/projects/[id]/environments/[envId]/flags/[flagId]", () => {
         ]);
 
         cy.findAllByText("New homepage").should("have.length", 3);
-        cy.findByRole("heading", { name: "Rollout details" }).should(
-          "be.visible"
-        );
+        cy.findByRole("heading", { name: "Overview" }).should("be.visible");
         cy.findByRole("heading", { name: "Percentage of the audience" }).should(
           "be.visible"
         );
