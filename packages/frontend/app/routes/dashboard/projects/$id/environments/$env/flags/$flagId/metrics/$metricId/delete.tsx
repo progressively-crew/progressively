581da672da69705d75b0b5e3a86131a2f6afa033
mfrachet <marvin.frachet@strapi.io>
2022-10-18 11:52:49 +0200

    delete page

---
+++ packages/frontend/app/routes/dashboard/projects/$id/environments/$env/flags/$flagId/metrics/$metricId/delete.tsx
@@ -0,0 +1,139 @@
+import { ErrorBox } from "~/components/Boxes/ErrorBox";
+import { WarningBox } from "~/components/Boxes/WarningBox";
+import { getSession } from "~/sessions";
+import { Button } from "~/components/Buttons/Button";
+import { DeleteEntityLayout } from "~/layouts/DeleteEntityLayout";
+import { DeleteButton } from "~/components/Buttons/DeleteButton";
+import { MetaFunction, ActionFunction, redirect } from "@remix-run/node";
+import { useActionData, Form, useTransition } from "@remix-run/react";
+import { useProject } from "~/modules/projects/contexts/useProject";
+import { useUser } from "~/modules/user/contexts/useUser";
+import { getProjectMetaTitle } from "~/modules/projects/services/getProjectMetaTitle";
+import { useEnvironment } from "~/modules/environments/contexts/useEnvironment";
+import { getEnvMetaTitle } from "~/modules/environments/services/getEnvMetaTitle";
+import { useFlagEnv } from "~/modules/flags/contexts/useFlagEnv";
+import { getFlagMetaTitle } from "~/modules/flags/services/getFlagMetaTitle";
+import { PageTitle } from "~/components/PageTitle";
+import { Stack } from "~/components/Stack";
+import { Typography } from "~/components/Typography";
+import { Header } from "~/components/Header";
+import { FlagIcon } from "~/components/Icons/FlagIcon";
+import { TagLine } from "~/components/Tagline";
+import { deleteMetric } from "~/modules/flags/services/deleteMetric";
+
+export const handle = {
+  breadcrumb: (match: { params: any }) => {
+    return {
+      link: `/dashboard/projects/${match.params.id}/environments/${match.params.env}/flags/${match.params.flagId}/metrics/${match.params.metricId}/delete`,
+      label: "Delete a metric",
+    };
+  },
+};
+
+export const meta: MetaFunction = ({ parentsData, params }) => {
+  const projectName = getProjectMetaTitle(parentsData);
+  const envName = getEnvMetaTitle(parentsData, params.env);
+  const flagName = getFlagMetaTitle(parentsData);
+
+  return {
+    title: `Progressively | ${projectName} | ${envName} | ${flagName} | Metrics | Delete`,
+  };
+};
+
+interface ActionData {
+  errors?: {
+    backendError?: string;
+  };
+}
+
+export const action: ActionFunction = async ({
+  request,
+  params,
+}): Promise<ActionData | Response> => {
+  const session = await getSession(request.headers.get("Cookie"));
+  const projectId = params.id!;
+  const envId = params.env!;
+  const flagId = params.flagId!;
+
+  try {
+    await deleteMetric(
+      envId,
+      flagId,
+      params.metricId!,
+      session.get("auth-cookie")
+    );
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return { errors: { backendError: error.message } };
+    }
+
+    return { errors: { backendError: "An error ocurred" } };
+  }
+
+  return redirect(
+    `/dashboard/projects/${projectId}/environments/${envId}/flags/${flagId}/metrics?metricRemoved=true#metric-removed`
+  );
+};
+
+export default function DeleteMetricPage() {
+  const transition = useTransition();
+  const data = useActionData<ActionData>();
+  const { project } = useProject();
+  const { user } = useUser();
+  const { environment } = useEnvironment();
+  const { flagEnv } = useFlagEnv();
+
+  const currentFlag = flagEnv.flag;
+
+  return (
+    <DeleteEntityLayout
+      user={user}
+      header={
+        <Header
+          tagline={<TagLine icon={<FlagIcon />}>FEATURE FLAG</TagLine>}
+          title={currentFlag.name}
+        />
+      }
+      error={
+        data?.errors &&
+        data.errors.backendError && <ErrorBox list={data.errors} />
+      }
+      cancelAction={
+        <Button
+          variant="secondary"
+          to={`/dashboard/projects/${project.uuid}/environments/${environment.uuid}/flags/${currentFlag.uuid}/metrics`}
+        >
+          {`No, don't delete`}
+        </Button>
+      }
+      confirmAction={
+        <Form method="post">
+          <DeleteButton
+            variant="primary"
+            scheme=""
+            type="submit"
+            isLoading={transition.state === "submitting"}
+            loadingText="Deleting the metric, please wait..."
+          >
+            Yes, delete the metric
+          </DeleteButton>
+        </Form>
+      }
+    >
+      <PageTitle value={`Deleting a metric`} />
+      <Stack spacing={4}>
+        <WarningBox title={<>This operation is definitive.</>} />
+
+        <Typography color="hadesLight">
+          If you validate the suppression, the metric will be removed from the
+          feature flag.
+        </Typography>
+
+        <Typography color="hadesLight">
+          All the metric related data (including events) will be removed and not
+          available anymore.
+        </Typography>
+      </Stack>
+    </DeleteEntityLayout>
+  );
+}
