be057085829e6241b63419262dab13576e0ea395
mfrachet <marvin.frachet@strapi.io>
2022-11-03 08:30:36 +0100

    iterate

--- packages/frontend/app/components/Fields/Label.tsx
+++ packages/frontend/app/components/Fields/Label.tsx
@@ -8,7 +8,7 @@ export const Label = ({ children, as: asComponent, htmlFor }: LabelProps) => {
   const Component = asComponent || "label";
 
   return (
-    <Component id="content" htmlFor={htmlFor} className="font-semibold">
+    <Component htmlFor={htmlFor} className="font-semibold">
       {children}
     </Component>
   );
