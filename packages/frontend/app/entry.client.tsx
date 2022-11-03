edac06506fac04fc56cbf7be0610ff0f9c35e03c
mfrachet <marvin.frachet@strapi.io>
2022-11-03 08:50:31 +0100

    feat(streaming): finally able to use renderToPipeableStream

--- packages/frontend/app/entry.client.tsx
+++ packages/frontend/app/entry.client.tsx
@@ -1,7 +1,25 @@
 import { RemixBrowser } from "@remix-run/react";
-import { hydrate } from "react-dom";
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
 import { initSentryOnClient } from "./modules/sentry/client";
 
 initSentryOnClient();
 
-hydrate(<RemixBrowser />, document);
+function hydrate() {
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <RemixBrowser />
+      </StrictMode>
+    );
+  });
+}
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate);
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  window.setTimeout(hydrate, 1);
+}
--- packages/frontend/app/entry.server.tsx
+++ packages/frontend/app/entry.server.tsx
@@ -1,10 +1,14 @@
+import { PassThrough } from "node:stream";
 import type { EntryContext } from "@remix-run/node";
+import { Response } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
-import { renderToString } from "react-dom/server";
-import { startMockServer } from "./mocks/mock-server";
+import isbot from "isbot";
+import { renderToPipeableStream } from "react-dom/server";
 import { initSentryOnServer } from "./modules/sentry/server";
 
 initSentryOnServer();
+
+const ABORT_DELAY = 5000;
 
 export default function handleRequest(
   request: Request,
@@ -12,22 +16,99 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  if (process.env.START_MOCK_SERVER === "true") {
-    console.log(
-      "[Mock server initiated] Value is:",
-      process.env.START_MOCK_SERVER
+  return isbot(request.headers.get("user-agent"))
+    ? handleBotRequest(
+        request,
+        responseStatusCode,
+        responseHeaders,
+        remixContext
+      )
+    : handleBrowserRequest(
+        request,
+        responseStatusCode,
+        responseHeaders,
+        remixContext
+      );
+}
+
+function handleBotRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  return new Promise((resolve, reject) => {
+    let didError = false;
+
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer context={remixContext} url={request.url} />,
+      {
+        onAllReady() {
+          const body = new PassThrough();
+
+          responseHeaders.set("Content-Type", "text/html");
+
+          resolve(
+            new Response(body, {
+              headers: responseHeaders,
+              status: didError ? 500 : responseStatusCode,
+            })
+          );
+
+          pipe(body);
+        },
+        onShellError(error: unknown) {
+          reject(error);
+        },
+        onError(error: unknown) {
+          didError = true;
+
+          console.error(error);
+        },
+      }
     );
-    startMockServer();
-  }
 
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
+    setTimeout(abort, ABORT_DELAY);
+  });
+}
 
-  responseHeaders.set("Content-Type", "text/html");
+function handleBrowserRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  return new Promise((resolve, reject) => {
+    let didError = false;
 
-  return new Response(`<!DOCTYPE html>${markup}`, {
-    status: responseStatusCode,
-    headers: responseHeaders,
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer context={remixContext} url={request.url} />,
+      {
+        onShellReady() {
+          const body = new PassThrough();
+
+          responseHeaders.set("Content-Type", "text/html");
+
+          resolve(
+            new Response(body, {
+              headers: responseHeaders,
+              status: didError ? 500 : responseStatusCode,
+            })
+          );
+
+          pipe(body);
+        },
+        onShellError(err: unknown) {
+          reject(err);
+        },
+        onError(error: unknown) {
+          didError = true;
+
+          console.error(error);
+        },
+      }
+    );
+
+    setTimeout(abort, ABORT_DELAY);
   });
 }
