77e3dca70330a2b008b7d51b8d9f0ac028086cb4
mfrachet <marvin.frachet@strapi.io>
2022-11-08 07:45:03 +0100

    not that bad

--- packages/documentation/app/components/Example.tsx
+++ packages/documentation/app/components/Example.tsx
@@ -82,7 +82,7 @@ export const Example = () => {
         </div>
       </div>
 
-      <div className="grid md:grid-cols-2 grid-cols-3 gap-4">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
         {browsers.map((_, index: number) => (
           <BrowserExample
             key={`browser-${index}`}
