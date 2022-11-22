c634daef7a2cacc3a4281b07f9aa12836e1fa4fe
mfrachet <marvin.frachet@strapi.io>
2022-11-22 14:09:13 +0100

    styles(switch): bring switch color back

--- packages/frontend/app/components/Switch.tsx
+++ packages/frontend/app/components/Switch.tsx
@@ -23,7 +23,7 @@ export const Switch = ({
     ? "translate-x-full text-emerald-200 flex items-center justify-center text-md"
     : "";
 
-  const wrapperCheckedClasses = checked ? "bg-green-300" : "bg-gray-300";
+  const wrapperCheckedClasses = checked ? "bg-emerald-300" : "bg-gray-300";
   const thumnailTransitionClasses = checked ? "group-active:-ml-4" : "";
 
   return (
@@ -39,7 +39,7 @@ export const Switch = ({
       <span className="text-gray-600">{offLabel || "Off"}</span>
       <span
         className={
-          "transition-all ease-in-out duration-200 h-7 w-12 bg-gray-300 rounded-full inline-block p-1 " +
+          "transition-all ease-in-out duration-200 h-7 w-12 rounded-full inline-block p-1 " +
           wrapperCheckedClasses
         }
       >
