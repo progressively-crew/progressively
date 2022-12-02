378c3ca8e26b12e882e12f8a6c772f58ab74d603
Marvin Frachet <marvin.frachet@protonmail.com>
2022-12-02 07:24:53 +0100

    fix(banner): add close banner (#336)

--- packages/frontend/app/components/Boxes/SuccessBox.tsx
+++ packages/frontend/app/components/Boxes/SuccessBox.tsx
@@ -1,5 +1,7 @@
+import { Link, useLocation } from "@remix-run/react";
 import { useEffect, useRef } from "react";
 import { AiOutlineCheckCircle } from "react-icons/ai";
+import { MdClose } from "react-icons/md";
 import { HStack } from "../HStack";
 
 export interface SuccessBoxProps {
@@ -9,23 +11,24 @@ export interface SuccessBoxProps {
 
 export const SuccessBox = ({ children, id, ...props }: SuccessBoxProps) => {
   const boxRef = useRef<HTMLDivElement>(null);
+  const location = useLocation();
 
   useEffect(() => {
     boxRef?.current?.focus();
   }, []);
 
   return (
-    <p
-      ref={boxRef}
-      tabIndex={-1}
-      id={id}
-      className="success-box p-4 bg-green-100 text-green-700 rounded border-l-8 border-l-green-600"
-      {...props}
-    >
-      <HStack as="span" spacing={2}>
-        <AiOutlineCheckCircle aria-hidden />
-        <span>{children}</span>
-      </HStack>
-    </p>
+    <div className="p-4 bg-green-100 text-green-700 rounded border-l-8 border-l-green-600 flex flex-row justify-between">
+      <p ref={boxRef} tabIndex={-1} id={id} {...props} className="success-box">
+        <HStack as="span" spacing={2}>
+          <AiOutlineCheckCircle aria-hidden />
+          <span>{children}</span>
+        </HStack>
+      </p>
+
+      <Link to={location.pathname} className="text-xl">
+        <MdClose aria-label="Close the banner" />
+      </Link>
+    </div>
   );
 };
