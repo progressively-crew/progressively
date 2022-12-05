5616f0217cbb3d9ceb27abd402a78e38966d0b9e
Marvin Frachet <marvin.frachet@protonmail.com>
2022-12-05 09:42:46 +0100

    chore(scheduling): order ASC in dashboard (#341)

--- packages/backend/src/scheduling/scheduling.service.ts
+++ packages/backend/src/scheduling/scheduling.service.ts
@@ -28,6 +28,9 @@ export class SchedulingService {
         flagEnvironmentEnvironmentId: envId,
         flagEnvironmentFlagId: flagId,
       },
+      orderBy: {
+        utc: 'asc',
+      },
     });
   }
 
