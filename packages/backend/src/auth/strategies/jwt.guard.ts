06d13a6db973244fda9903884f55c85b3074d193
mfrachet <marvin.frachet@strapi.io>
2022-12-06 09:20:44 +0100

    okta authentication backend

--- packages/backend/src/auth/strategies/jwt.guard.ts
+++ packages/backend/src/auth/strategies/jwt.guard.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { OktaService } from '../okta.service';
+
+const guards = OktaService.getOktaConfig().isOktaActivated
+  ? ['jwt', 'bearer']
+  : 'jwt';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard(['jwt', 'bearer']) {}
+export class JwtAuthGuard extends AuthGuard(guards) {}
