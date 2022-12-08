1c24fd19f2af86898904dba8b4d1614e412b4279
mfrachet <marvin.frachet@strapi.io>
2022-12-08 16:42:25 +0100

    wip

--- packages/documentation/app/routes/recipes/$id.tsx
+++ packages/documentation/app/routes/recipes/$id.tsx
@@ -4,7 +4,7 @@ import { asHTML } from "@prismicio/helpers";
 import { GiCook } from "react-icons/gi";
 import { client } from "~/modules/prismic/client";
 import type { RecipePostDocument } from "types.generated";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import { Title } from "~/components/Title";
 import hljs from "highlight.js";
 import { useEffect } from "react";
@@ -22,6 +22,8 @@ export const loader: LoaderFunction = async ({
   try {
     const pageData = await client.getByUID("recipe_post", params.id!);
 
+    let headerIndex = 0;
+
     const htmlSerializer: HTMLFunctionSerializer = (
       type,
       element,
@@ -30,6 +32,20 @@ export const loader: LoaderFunction = async ({
     ) => {
       if (type === "preformatted")
         return `<pre><code class="hljs language-js">${children}</code></pre>`;
+
+      let nextId = "";
+
+      if (type.includes("heading")) {
+        nextId = `heading-${headerIndex}`;
+        headerIndex += 1;
+      }
+
+      if (type === "heading2") return `<h2 id="${nextId}">${children}</h2>`;
+      if (type === "heading3") return `<h3 id="${nextId}">${children}</h3>`;
+      if (type === "heading4") return `<h4 id="${nextId}">${children}</h4>`;
+      if (type === "heading5") return `<h5 id="${nextId}">${children}</h5>`;
+      if (type === "heading6") return `<h6 id="${nextId}">${children}</h6>`;
+
       return null;
     };
 
@@ -68,7 +84,7 @@ export default function RecipePost() {
           <Title value={pageData.data.title} />
         </div>
 
-        <div className="grid grid-cols-[300px_1fr] gap-8 items-start pt-8">
+        <div className="grid grid-cols-[300px_1fr_300px] gap-8 items-start pt-8">
           <div className="sticky top-8">
             <SimpleCard>
               <CardContent>
@@ -92,7 +108,43 @@ export default function RecipePost() {
                 <aside>
                   <h2 className="font-semibold">Directions</h2>
                   <ul className="pt-4">
-                    {pageData.data.directions.map((ing) => (
+                    {pageData.data.directions.map((ing, index: number) => (
+                      <li key={ing.name[0]?.text} className="text-xs py-1">
+                        <Link
+                          to={`#heading-${index}`}
+                          className="text-indigo-700 underline hover:text-indigo-600 active:text-indigo-800"
+                        >
+                          {ing.name[0]?.text}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </aside>
+              </CardContent>
+            </SimpleCard>
+          </div>
+
+          <main className="prose lg:prose-x overflow-hidden max-w-none">
+            <div>
+              {imgUrl && (
+                <img
+                  src={imgUrl}
+                  alt={pageData.data.title[0]?.text}
+                  className="mt-0 object-cover"
+                  height="400px"
+                />
+              )}
+              <div dangerouslySetInnerHTML={{ __html: html }} />
+            </div>
+          </main>
+
+          <div className="sticky top-8">
+            <SimpleCard>
+              <CardContent>
+                <aside>
+                  <h2 className="font-semibold">Meta</h2>
+                  <ul className="list-disc list-inside pt-4">
+                    {pageData.data.ingredients.map((ing) => (
                       <li key={ing.name[0]?.text} className="text-xs py-1">
                         {ing.name[0]?.text}
                       </li>
@@ -102,20 +154,6 @@ export default function RecipePost() {
               </CardContent>
             </SimpleCard>
           </div>
-
-          <main className="prose lg:prose-x">
-            <div>
-              {imgUrl && (
-                <img
-                  src={imgUrl}
-                  alt={pageData.data.title[0]?.text}
-                  className="mt-0"
-                  style={{ height: 264 }}
-                />
-              )}
-              <div dangerouslySetInnerHTML={{ __html: html }} />
-            </div>
-          </main>
         </div>
       </div>
     </Background>
