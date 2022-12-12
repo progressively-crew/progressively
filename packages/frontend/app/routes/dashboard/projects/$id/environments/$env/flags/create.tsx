378954a3727b735bdcd037c50d7bfee1f0d72fb5
mfrachet <marvin.frachet@strapi.io>
2022-12-12 17:03:17 +0100

    wip

--- packages/frontend/app/routes/dashboard/projects/$id/environments/$env/flags/create.tsx
+++ packages/frontend/app/routes/dashboard/projects/$id/environments/$env/flags/create.tsx
@@ -4,37 +4,21 @@ import { SubmitButton } from "~/components/Buttons/SubmitButton";
 import { ErrorBox } from "~/components/Boxes/ErrorBox";
 import { FormGroup } from "~/components/Fields/FormGroup";
 import { TextInput } from "~/components/Fields/TextInput";
-import { Section } from "~/components/Section";
-import { Typography } from "~/components/Typography";
 import { createFlag } from "~/modules/flags/services/createFlag";
 import { CreateFlagDTO, Flag, FlagType } from "~/modules/flags/types";
 import { validateFlagShape } from "~/modules/flags/validators/validateFlagShape";
 import { getSession } from "~/sessions";
 import { useProject } from "~/modules/projects/contexts/useProject";
-import { useUser } from "~/modules/user/contexts/useUser";
 import { getProjectMetaTitle } from "~/modules/projects/services/getProjectMetaTitle";
 import { useEnvironment } from "~/modules/environments/contexts/useEnvironment";
 import { getEnvMetaTitle } from "~/modules/environments/services/getEnvMetaTitle";
-import { PageTitle } from "~/components/PageTitle";
-import { Card, CardContent } from "~/components/Card";
 import { SelectField } from "~/components/Fields/SelectField";
-import { Header } from "~/components/Header";
-import { EnvIcon } from "~/components/Icons/EnvIcon";
-import { TagLine } from "~/components/Tagline";
 import { ReleaseIcon } from "~/components/Icons/ReleaseIcon";
 import { ExperimentIcon } from "~/components/Icons/ExperimentIcon";
 import { PermissionIcon } from "~/components/Icons/PermissionIcon";
 import { KillSwitchIcon } from "~/components/Icons/KillSwitchIcon";
 import { CreateEntityLayout } from "~/layouts/CreateEntityLayout";
-
-export const handle = {
-  breadcrumb: (match: { params: any }) => {
-    return {
-      link: `/dashboard/projects/${match.params.id}/environments/${match.params.env}/flags/create`,
-      label: "Create a feature flag",
-    };
-  },
-};
+import { BackLink } from "~/components/BackLink";
 
 export const meta: MetaFunction = ({ params, parentsData }) => {
   const projectName = getProjectMetaTitle(parentsData);
@@ -91,7 +75,6 @@ export const action: ActionFunction = async ({
 
 export default function CreateFlagPage() {
   const { project } = useProject();
-  const { user } = useUser();
   const data = useActionData<ActionData>();
   const transition = useTransition();
   const { environment } = useEnvironment();
@@ -99,95 +82,77 @@ export default function CreateFlagPage() {
   const errors = data?.errors;
 
   return (
-    <CreateEntityLayout
-      user={user}
-      header={
-        <Header
-          tagline={<TagLine icon={<EnvIcon />}>ENVIRONMENT</TagLine>}
-          title={environment.name}
-        />
-      }
-      status={
-        (errors?.name || errors?.description) && <ErrorBox list={errors} />
-      }
-      title={
-        <PageTitle
-          value="Create a feature flag"
-          description={
-            <Typography>
-              The new feature flag will appear in{" "}
-              <strong>{project.name}</strong> /{" "}
-              <strong>{environment.name}</strong>. After the creation of a
-              feature flag, you will be able to get its SDK key for application
-              usage.
-            </Typography>
-          }
-        />
-      }
-    >
-      <Card>
-        <CardContent>
-          <Section>
-            <Form method="post">
-              <FormGroup>
-                <TextInput
-                  name="flag-name"
-                  isInvalid={Boolean(errors?.name)}
-                  label="Flag name"
-                  placeholder="e.g: New Homepage"
-                />
+    <Form method="post">
+      <CreateEntityLayout
+        status={
+          (errors?.name || errors?.description) && <ErrorBox list={errors} />
+        }
+        titleSlot={
+          <h1 className="text-3xl font-semibold" id="page-title">
+            Create a feature flag
+          </h1>
+        }
+        submitSlot={
+          <SubmitButton
+            type="submit"
+            isLoading={transition.state === "submitting"}
+            loadingText="Creating the feature flag, please wait..."
+          >
+            Create the feature flag
+          </SubmitButton>
+        }
+        backLinkSlot={
+          <BackLink
+            to={`/dashboard/projects/${project.uuid}/environments/${environment.uuid}`}
+          >
+            Back to environment
+          </BackLink>
+        }
+      >
+        <FormGroup>
+          <TextInput
+            name="flag-name"
+            isInvalid={Boolean(errors?.name)}
+            label="Flag name"
+            placeholder="e.g: New Homepage"
+          />
 
-                <TextInput
-                  name="flag-desc"
-                  isInvalid={Boolean(errors?.description)}
-                  label="Flag description"
-                  placeholder="e.g: The new homepage"
-                />
+          <TextInput
+            name="flag-desc"
+            isInvalid={Boolean(errors?.description)}
+            label="Flag description"
+            placeholder="e.g: The new homepage"
+          />
 
-                <div>
-                  <SelectField
-                    isInvalid={Boolean(errors?.type)}
-                    name="type"
-                    label="Type"
-                    options={[
-                      {
-                        value: FlagType.RELEASE,
-                        label: "Release",
-                        icon: <ReleaseIcon />,
-                      },
-                      {
-                        value: FlagType.EXPERIMENT,
-                        label: "Experiment",
-                        icon: <ExperimentIcon />,
-                      },
-                      {
-                        value: FlagType.PERMISSION,
-                        label: "Permission",
-                        icon: <PermissionIcon />,
-                      },
-                      {
-                        value: FlagType.KILL_SWITCH,
-                        label: "Kill switch",
-                        icon: <KillSwitchIcon />,
-                      },
-                    ]}
-                  />
-                </div>
-
-                <div>
-                  <SubmitButton
-                    type="submit"
-                    isLoading={transition.state === "submitting"}
-                    loadingText="Creating the feature flag, please wait..."
-                  >
-                    Create the feature flag
-                  </SubmitButton>
-                </div>
-              </FormGroup>
-            </Form>
-          </Section>
-        </CardContent>
-      </Card>
-    </CreateEntityLayout>
+          <SelectField
+            isInvalid={Boolean(errors?.type)}
+            name="type"
+            label="Type"
+            options={[
+              {
+                value: FlagType.RELEASE,
+                label: "Release",
+                icon: <ReleaseIcon />,
+              },
+              {
+                value: FlagType.EXPERIMENT,
+                label: "Experiment",
+                icon: <ExperimentIcon />,
+              },
+              {
+                value: FlagType.PERMISSION,
+                label: "Permission",
+                icon: <PermissionIcon />,
+              },
+              {
+                value: FlagType.KILL_SWITCH,
+                label: "Kill switch",
+                icon: <KillSwitchIcon />,
+              },
+            ]}
+          />
+        </FormGroup>
+      </CreateEntityLayout>
+    </Form>
   );
 }
