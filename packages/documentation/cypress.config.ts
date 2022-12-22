7672a7209f0b0d4ef96aa3ceb59c0772a8041b58
mfrachet <marvin.frachet@strapi.io>
2022-12-22 06:41:58 +0100

    fix(responsive): color were not working as expected

--- packages/documentation/cypress.config.ts
+++ packages/documentation/cypress.config.ts
@@ -3,7 +3,18 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("before:browser:launch", (browser, launchOptions) => {
+        const REDUCE = 1;
+        if (browser.family === "firefox") {
+          launchOptions.preferences["ui.prefersReducedMotion"] = REDUCE;
+        }
+
+        if (browser.family === "chromium") {
+          launchOptions.args.push("--force-prefers-reduced-motion");
+        }
+
+        return launchOptions;
+      }),
     },
   },
 });
