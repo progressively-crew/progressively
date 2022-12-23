1759dc45dd432c4865a1a6c3d3fb383fc9f83113
mfrachet <marvin.frachet@strapi.io>
2022-12-23 07:59:36 +0100

    refacto(remove): flag type

---
+++ packages/backend/prisma/migrations/20221223065106_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `type` on the `Flag` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Flag" DROP COLUMN "type";
