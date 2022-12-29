d6f822ed6985f14b614f104cf06b67806f96d334
mfrachet <marvin.frachet@strapi.io>
2022-12-29 19:47:56 +0100

    make sure load testing uses the local cypress helpers

--- packages/frontend/app/modules/insights/components/EvalCard.tsx
+++ packages/frontend/app/modules/insights/components/EvalCard.tsx
@@ -8,9 +8,7 @@ export const EvalCard = ({ count }: EvalCardProps) => {
       className="border border-gray-100 rounded-md dark:border-slate-700 p-4 flex flex-col items-center justify-center bg-gradient-to-t from-indigo-600 to-pink-400"
       style={{ height: 300 }}
     >
-      <div className="text-6xl dark:text-slate-100 text-slate-900 font-bold">
-        {count}
-      </div>
+      <div className="text-6xl text-slate-100 font-bold">{count}</div>
       <p className="text-lg text-slate-100 text-xl">evaluations</p>
     </div>
   );
