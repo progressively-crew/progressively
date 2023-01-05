421d6f633282fd9c75f25798a6f5e93b279de8d8
Marvin Frachet <marvin.frachet@protonmail.com>
2023-01-05 09:01:41 +0100

    chore(deps): factorize and update cypress in example folders (#400)

---
+++ example/svelte/e2e/index.js
@@ -0,0 +1,48 @@
+import { FlagStatus } from "../../../packages/backend/src/flags/flags.status";
+
+describe("/", () => {
+  beforeEach(cy.seed);
+
+  afterEach(() => {
+    cy.wait(500);
+    cy.cleanup();
+  });
+
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("shows the old variant when the flag is not activated", () => {
+    cy.findByText("Old variant").should("be.visible");
+    cy.findByText("New variant").should("not.exist");
+
+    cy.findByText("New footer").should("be.visible");
+    cy.findByText("Old footer").should("not.exist");
+  });
+
+  it("shows the new variant when the flag is activated for homepage", () => {
+    cy.changeFlagStatus("1", "1", FlagStatus.ACTIVATED);
+
+    // Verify the activation using sockets
+    cy.findByText("Old variant").should("not.exist");
+    cy.findByText("New variant").should("be.visible");
+
+    // Verify the activation using SSR
+    cy.reload();
+    cy.findByText("Old variant").should("not.exist");
+    cy.findByText("New variant").should("be.visible");
+  });
+
+  it("shows the old variant when the flag is activated for footer (by default activated)", () => {
+    cy.changeFlagStatus("1", "2", FlagStatus.NOT_ACTIVATED);
+
+    // Verify the activation using sockets
+    cy.findByText("New variant").should("not.exist");
+    cy.findByText("Old variant").should("be.visible");
+
+    // Verify the activation using SSR
+    cy.reload();
+    cy.findByText("New footer").should("not.exist");
+    cy.findByText("Old footer").should("be.visible");
+  });
+});
