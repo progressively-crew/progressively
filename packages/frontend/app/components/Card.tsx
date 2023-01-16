be703100cd1e3879452deb813d2df54b5831a114
mfrachet <marvin.frachet@strapi.io>
2023-01-16 16:50:31 +0100

    wip

--- packages/frontend/app/components/Card.tsx
+++ packages/frontend/app/components/Card.tsx
@@ -1,11 +1,16 @@
+import React from "react";
+
 export interface CardProps {
   children: React.ReactNode;
+  footer?: React.ReactNode;
 }
 
-export const Card = ({ children }: CardProps) => {
+export const Card = ({ children, footer }: CardProps) => {
   return (
-    <div className="border border-gray-100 rounded-md bg-white dark:border-slate-700 dark:bg-slate-800 overflow-hidden">
+    <div className="border border-gray-200 rounded-md bg-white dark:border-slate-700 dark:bg-slate-800 overflow-hidden">
       {children}
+
+      {footer && <div className="bg-gray-50 px-6 py-4">{footer}</div>}
     </div>
   );
 };
@@ -14,5 +19,5 @@ export interface CardContentProps {
   children: React.ReactNode;
 }
 export const CardContent = ({ children }: CardContentProps) => {
-  return <div className="p-8">{children}</div>;
+  return <div className="p-6">{children}</div>;
 };
