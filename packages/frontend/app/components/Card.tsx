5d55ad9e3ef14ef9ff70223c334ad2fa8ba34610
mfrachet <marvin.frachet@strapi.io>
2023-01-16 18:12:47 +0100

    wip

--- packages/frontend/app/components/Card.tsx
+++ packages/frontend/app/components/Card.tsx
@@ -10,7 +10,9 @@ export const Card = ({ children, footer }: CardProps) => {
     <div className="border border-gray-200 rounded-md bg-white dark:border-slate-700 dark:bg-slate-800 overflow-hidden">
       {children}
 
-      {footer && <div className="bg-gray-50 px-6 py-4">{footer}</div>}
+      {footer && (
+        <div className="bg-gray-50 px-6 py-4 dark:bg-slate-700">{footer}</div>
+      )}
     </div>
   );
 };
