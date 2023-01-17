0b35038ba4350156de56609ded06b5c719af6dc6
mfrachet <marvin.frachet@strapi.io>
2023-01-17 11:36:43 +0100

    Revert .env

--- packages/frontend/app/components/Card.tsx
+++ packages/frontend/app/components/Card.tsx
@@ -3,11 +3,19 @@ import React from "react";
 export interface CardProps {
   children: React.ReactNode;
   footer?: React.ReactNode;
+  onClick?: () => void;
 }
 
-export const Card = ({ children, footer }: CardProps) => {
+export const Card = ({ children, footer, onClick }: CardProps) => {
+  const clickableClasses = onClick
+    ? "hover:bg-gray-50 hover:dark:bg-slate-700 active:bg-gray-100 active:dark:bg-slate-600 cursor-pointer"
+    : "";
+
   return (
-    <div className="border border-gray-200 rounded-md bg-white dark:border-slate-700 dark:bg-slate-800 overflow-hidden">
+    <div
+      className={`border border-gray-200 rounded-md bg-white dark:border-slate-700 dark:bg-slate-800 overflow-hidden ${clickableClasses}`}
+      onClick={onClick}
+    >
       {children}
 
       {footer && (
--- packages/frontend/app/modules/projects/components/ProjectList.tsx
+++ packages/frontend/app/modules/projects/components/ProjectList.tsx
@@ -18,40 +18,38 @@ const ProjectCard = ({ userProject }: ProjectRowProps) => {
   const linkRef = useRef<HTMLAnchorElement>(null);
 
   return (
-    <div onClick={() => linkRef.current?.click()} className="cursor-pointer">
-      <Card>
-        <CardContent>
-          <div className="flex flex-row gap-4 items-center">
-            <div
-              className="w-12 h-12 text-xl rounded flex items-center justify-center"
-              aria-hidden
-              style={{
-                color: stringToColor(userProject.project.name, 25),
-                background: stringToColor(userProject.project.name, 90),
-              }}
-            >
-              {userProject.project.name[0]}
-            </div>
-
-            <div className="">
-              <Link
-                ref={linkRef}
-                to={`/dashboard/projects/${userProject.projectId}`}
-                className="dark:text-slate-100"
-              >
-                {userProject.project.name}
-              </Link>
-
-              <Spacer size={2} />
-
-              <Typography className="dark:text-slate-300 text-sm">
-                Role in the project: <Tag size="S">{userProject.role}</Tag>
-              </Typography>
-            </div>
+    <Card onClick={() => linkRef.current?.click()}>
+      <CardContent>
+        <div className="flex flex-row gap-4 items-center">
+          <div
+            className="w-12 h-12 text-xl rounded flex items-center justify-center"
+            aria-hidden
+            style={{
+              color: stringToColor(userProject.project.name, 25),
+              background: stringToColor(userProject.project.name, 90),
+            }}
+          >
+            {userProject.project.name[0]}
           </div>
-        </CardContent>
-      </Card>
-    </div>
+
+          <div className="">
+            <Link
+              ref={linkRef}
+              to={`/dashboard/projects/${userProject.projectId}`}
+              className="dark:text-slate-100"
+            >
+              {userProject.project.name}
+            </Link>
+
+            <Spacer size={2} />
+
+            <Typography className="dark:text-slate-300 text-sm">
+              Role in the project: <Tag size="S">{userProject.role}</Tag>
+            </Typography>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 export const ProjectList = ({ projects }: ProjectListProps) => {
