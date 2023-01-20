2976d96fbbc4aa48f37a19a11a8b83f3f6d57099
mfrachet <marvin.frachet@strapi.io>
2023-01-20 10:01:58 +0100

    fix be test

--- packages/frontend/cypress/e2e/projects/id/environments/envId/flags/flagId/index.spec.ts
+++ packages/frontend/cypress/e2e/projects/id/environments/envId/flags/flagId/index.spec.ts
@@ -29,11 +29,12 @@ describe("/dashboard/projects/[id]/environments/[envId]/flags/[flagId]", () => {
     describe("user: Marvin", () => {
       beforeEach(() => {
         cy.signIn("Marvin");
-        cy.visit("/dashboard/projects/1/environments/1/flags/1");
-        cy.injectAxe();
       });
 
       it("shows the layout", () => {
+        cy.visit("/dashboard/projects/1/environments/1/flags/1");
+        cy.injectAxe();
+
         cy.title().should(
           "eq",
           "Progressively | Project from seeding | Production | Flags | New homepage"
@@ -59,6 +60,43 @@ describe("/dashboard/projects/[id]/environments/[envId]/flags/[flagId]", () => {
         cy.findByRole("button", { name: "Update" }).should("not.exist");
 
         cy.checkA11y();
+      });
+
+      describe.only("Audience eligibility (form)", () => {
+        beforeEach(() => {
+          cy.visit("/dashboard/projects/1/environments/1/flags/2");
+          cy.injectAxe();
+        });
+
+        it("shows an initial form", () => {
+          cy.findByLabelText("Field name")
+            .should("be.visible")
+            .and("have.value", "email");
+          cy.findByLabelText("Field comparator")
+            .should("be.visible")
+            .and("have.value", "eq");
+          cy.get("[name='field-value']").and("have.value", "@gmail.com");
+
+          cy.checkA11y();
+        });
+
+        it("adds a new rule when pressing the button", () => {
+          cy.findAllByLabelText("Field name").should("have.length", 1);
+
+          cy.findByRole("button", { name: "Add a new rule" })
+            .should("be.visible")
+            .click();
+
+          cy.findAllByLabelText("Field name").should("have.length", 2);
+
+          cy.reload();
+
+          cy.findAllByLabelText("Field name")
+            .should("be.visible")
+            .and("have.length", 2);
+
+          cy.checkA11y();
+        });
       });
     });
   });
