dfedb413e23fcdb665acee69a7857a16d729770c
mfrachet <marvin.frachet@strapi.io>
2023-01-27 06:02:29 +0100

    wip

--- packages/frontend/app/components/Fields/TagInput.tsx
+++ packages/frontend/app/components/Fields/TagInput.tsx
@@ -38,8 +38,11 @@ export const TagInput = ({ defaultValue }: TagInputProps) => {
     setTags((s) => s.filter((t) => t !== tag));
   };
 
+  const inputWrapperClasses =
+    "rounded w-full focus-within:outline-none focus-within:ring-2 focus-within:ring-slate-400 focus-within:ring-offset-2 dark:focus-within:ring-offset-slate-900";
+
   return (
-    <div className="w-full">
+    <div className={inputWrapperClasses}>
       <div className="min-h-10 rounded px-2 py-2 border border-gray-200 dark:border-slate-800 dark:text-slate-100 dark:bg-slate-700 flex flex-wrap gap-2 bg-white w-full text-gray-600 dark:text-slate-100">
         {tags.length > 0 && (
           <ul className="flex flex-row flex-wrap items-center gap-2 h-full items-center">
@@ -62,7 +65,7 @@ export const TagInput = ({ defaultValue }: TagInputProps) => {
           type="text"
           name="tag-name"
           aria-label="Add a new tag to the list"
-          className="bg-white dark:bg-transparent"
+          className="bg-white dark:bg-transparent outline-none"
           onKeyDown={handleKeyDown}
           value={value}
           onChange={(e) => setValue(e.target.value)}
