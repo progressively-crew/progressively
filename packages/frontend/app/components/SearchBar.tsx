391e4cc9a24998c8415e317931470a5e9450ee4c
mfrachet <marvin.frachet@strapi.io>
2023-01-27 06:08:23 +0100

    wip

--- packages/frontend/app/components/SearchBar.tsx
+++ packages/frontend/app/components/SearchBar.tsx
@@ -17,24 +17,22 @@ export const SearchBar = ({ placeholder, label, count }: SearchBarProps) => {
         <label htmlFor="search">{label}</label>
       </VisuallyHidden>
 
-      <Form role="search">
-        <div className="flex flex-row items-center h-10 rounded pl-4 border border-gray-200 gap-2 bg-white overflow-hidden w-full dark:border-slate-700 dark:bg-slate-800 pr-1">
-          <input
-            type="text"
-            name="search"
-            id="search"
-            placeholder={placeholder}
-            className="flex-1 dark:text-slate-100 dark:bg-slate-800"
-          />
+      <Form role="search" className="flex flex-row gap-4 items-center relative">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder={placeholder}
+          className="h-10 rounded pl-4 pr-12 border border-gray-200 dark:border-slate-700 dark:bg-slate-800 flex-1 dark:text-slate-100 dark:bg-slate-800 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:focus:ring-offset-slate-900"
+        />
 
-          <button
-            type="submit"
-            aria-label="Search"
-            className="w-8 h-8 rounded bg-indigo-100 text-indigo-700 text-indigo-700 hover:bg-indigo-50 active:bg-indigo-100 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:active:bg-slate-500 flex items-center justify-center"
-          >
-            <AiOutlineSearch />
-          </button>
-        </div>
+        <button
+          type="submit"
+          aria-label="Search"
+          className="right-1 absolute w-8 h-8 rounded bg-indigo-100 text-indigo-700 text-indigo-700 hover:bg-indigo-50 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:active:bg-slate-500 flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:focus:ring-offset-slate-900"
+        >
+          <AiOutlineSearch />
+        </button>
       </Form>
 
       {count === undefined ? null : <Spacer size={1} />}
