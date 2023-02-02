2fef3bc90afa74b53c6f5b9ac263973459eb8efa
mfrachet <marvin.frachet@strapi.io>
2023-02-02 22:16:20 +0100

    ui again

--- packages/frontend/app/modules/activity/components/ActivityType.tsx
+++ packages/frontend/app/modules/activity/components/ActivityType.tsx
@@ -10,31 +10,59 @@ export const ActivityType = ({ type }: ActivityTypeProps) => {
   }
 
   if (type === "change-flag-percentage") {
-    return <span>Flag percentage has been changed.</span>;
+    return (
+      <span>
+        <strong>Flag percentage</strong> has been changed.
+      </span>
+    );
   }
 
   if (type === "create-additional-audience") {
-    return <span>An additional audience has been created.</span>;
+    return (
+      <span>
+        An <strong>additional audience</strong> has been created.
+      </span>
+    );
   }
 
   if (type === "create-eligibility-restriction") {
-    return <span>An eligibility restriction has been created.</span>;
+    return (
+      <span>
+        An <strong>eligibility restriction</strong> has been created.
+      </span>
+    );
   }
 
   if (type === "create-webhook") {
-    return <span>A webhook has been created.</span>;
+    return (
+      <span>
+        <strong>A webhook</strong> has been created.
+      </span>
+    );
   }
 
   if (type === "create-scheduling") {
-    return <span>A scheduling has been created.</span>;
+    return (
+      <span>
+        <strong>A scheduling</strong> has been created.
+      </span>
+    );
   }
 
   if (type === "create-metric") {
-    return <span>A metric has been created.</span>;
+    return (
+      <span>
+        <strong>A metric</strong> has been created.
+      </span>
+    );
   }
 
   if (type === "create-variant") {
-    return <span>A variant has been created.</span>;
+    return (
+      <span>
+        <strong>A variant</strong> has been created.
+      </span>
+    );
   }
 
   return <span>Woops, the type {type} is not supported yet.</span>;
