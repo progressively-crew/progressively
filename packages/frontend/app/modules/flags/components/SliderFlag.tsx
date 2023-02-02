4e5a7d6388ec84d133501f8fbabe93d4dbcd56d0
mfrachet <marvin.frachet@strapi.io>
2023-02-02 07:46:00 +0100

    ui again

--- packages/frontend/app/modules/flags/components/SliderFlag.tsx
+++ packages/frontend/app/modules/flags/components/SliderFlag.tsx
@@ -1,9 +1,6 @@
 import { Form } from "@remix-run/react";
 import { useEffect, useState } from "react";
-import { SubmitButton } from "~/components/Buttons/SubmitButton";
 import { SliderInput } from "~/components/Fields/SliderInput";
-
-import { Stack } from "~/components/Stack";
 
 export interface SliderFlagProps {
   initialRolloutPercentage: number;
