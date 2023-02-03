78035c761ad7a09eb5bf46e7ef01130f3365de44
mfrachet <marvin.frachet@strapi.io>
2023-02-03 17:07:03 +0100

    wip

--- packages/documentation/remix.config.js
+++ packages/documentation/remix.config.js
@@ -8,7 +8,7 @@ const vercelSpecificConfig =
 
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*", "**/middleware.js"],
+  ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
