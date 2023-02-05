a6c446a90dd9e532f13d4c51e2fbe42bc3b79c09
mfrachet <marvin.frachet@strapi.io>
2023-02-05 14:52:36 +0100

    good

--- packages/frontend/app/modules/activity/components/ActivityLogList.tsx
+++ packages/frontend/app/modules/activity/components/ActivityLogList.tsx
@@ -15,11 +15,11 @@ const ActivityItem = ({ activity, flagEnv }: ActivityItemProps) => {
 
   return (
     <div
-      className={`border-t border-gray-200 dark:border-slate-700 py-4 text-sm text-gray-600 flex flex-row justify-between gap-4`}
+      className={`border-t border-gray-200 dark:border-slate-700 py-4 text-sm text-gray-600 flex flex-col md:flex-row md:justify-between md:gap-4`}
     >
       <div className="flex flex-row">
         <div
-          className="w-8 h-8 flex items-center justify-center rounded-full mr-4"
+          className="w-8 h-8 flex items-center justify-center rounded-full mr-4 shrink-0"
           style={{ background }}
         >
           <ActivityIcon type={activity.type} />
@@ -30,9 +30,11 @@ const ActivityItem = ({ activity, flagEnv }: ActivityItemProps) => {
         </div>
       </div>
 
-      <p className="flex flex-row gap-2 py-2 shrink-0 text-xs text-gray-500 dark:text-slate-300">
+      <p className="pl-12 md:pl-0 -mt-3 md:mt-0 flex flex-row gap-2 py-2 shrink-0 text-xs text-gray-500 dark:text-slate-300">
         <span>By {activity.user.fullname}</span>
-        <span className="w-24 text-right">{formatDateAgo(activity.utc)}</span>
+        <span className="md:w-24 text-right">
+          {formatDateAgo(activity.utc)}
+        </span>
       </p>
     </div>
   );
