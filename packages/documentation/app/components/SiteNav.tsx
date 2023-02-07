342245e50715f321c4feb425bf0a9413f7ba5315
mfrachet <marvin.frachet@strapi.io>
2023-02-07 07:34:36 +0100

    refactor(docs): move to new doc

--- packages/documentation/app/components/SiteNav.tsx
+++ packages/documentation/app/components/SiteNav.tsx
@@ -13,6 +13,7 @@ const NavLink = ({
 }) => {
   const className =
     "no-underline font-semibold hover:underline text-gray-800 active:text-gray-600 dark:text-slate-200 active:dark:text-slate-50";
+
   if (href) {
     return (
       <a
