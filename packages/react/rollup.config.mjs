b901c0c3c3dc6e4a50c6f6bcdcf13ba61acd037d
mfrachet <marvin.frachet@strapi.io>
2023-02-14 09:22:29 +0100

    use packages

--- packages/react/rollup.config.mjs
+++ packages/react/rollup.config.mjs
@@ -1,34 +1,41 @@
 import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+const external = ["react"];
+const globals = { react: "react" };
 
 export default () => {
   const legacy = {
     input: "src/index.tsx",
     output: {
-      name: "progressively-server-side",
+      name: "progressively-react",
       dir: "lib/legacy",
       format: "umd",
+      globals,
     },
     plugins: [
       nodeResolve(),
       typescript({ outDir: "lib/legacy", target: "es5" }),
       terser(),
     ],
+    external,
   };
 
   const esm = {
     input: "src/index.tsx",
     output: {
-      name: "progressively-server-side",
+      name: "progressively-react",
       dir: "lib/esm",
       format: "esm",
+      globals,
     },
     plugins: [
       nodeResolve(),
       typescript({ outDir: "lib/esm", target: "ESNext" }),
       terser(),
     ],
+    external,
   };
 
   return [legacy, esm];
