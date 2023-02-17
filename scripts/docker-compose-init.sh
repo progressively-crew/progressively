a1c85d6bfeb4e735d9c52c806a251f16c1aadb03
mfrachet <marvin.frachet@strapi.io>
2023-02-17 14:20:11 +0100

    fix

--- scripts/docker-compose-init.sh
+++ scripts/docker-compose-init.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 # only useful to create the DB tables in docker-compose.yml
-npm run db:migrate:dev
-npm run db:generate:schema
-npm run start
+pnpm run db:prepare
+pnpm run start:backend
