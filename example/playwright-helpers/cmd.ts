5ccfa28c9fda3c76bd9cfaba30694aa00579b099
Marvin Frachet <marvin.frachet@protonmail.com>
2023-02-20 08:27:05 +0100

    chore(playwright): move to playwright for examples (#457)

---
+++ example/playwright-helpers/cmd.ts
@@ -0,0 +1,32 @@
+import { cleanupDb, seedDb } from "@progressively/database/seed";
+
+export const changeFlagStatus = (
+  envId: string,
+  flagId: string,
+  status: string
+) => {
+  return fetch("http://localhost:4000/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: "marvin.frachet@something.com",
+      password: "password",
+    }),
+  })
+    .then((res) => res.json())
+    .then(({ access_token }) =>
+      fetch(`http://localhost:4000/environments/${envId}/flags/${flagId}`, {
+        method: "PUT",
+        body: JSON.stringify({
+          status,
+        }),
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          "Content-Type": "application/json",
+        },
+      })
+    );
+};
+
+export const seed = seedDb;
+export const cleanup = cleanupDb;
