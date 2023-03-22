de1e224b366c295f439f0eb7cc73608e53c7a110
mfrachet <undefined>
2023-03-22 14:48:14 +0100

    refactor(deps): remove unused jest references

--- packages/frontend/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  rootDir: "app",
-  moduleNameMapper: {
-    "~/(.*)$": "<rootDir>/$1",
-  },
-  testEnvironment: "jsdom",
-};
