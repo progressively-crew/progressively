af86150b37abf94b01092907fb582c75dd4c65d4
mfrachet <undefined>
2023-03-22 07:05:19 +0100

    refactor(test): vitest instead of jest in react package

--- packages/react/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  rootDir: "src",
-  moduleNameMapper: {
-    "@progressively/sdk-js":
-      "<rootDir>/../node_modules/@progressively/sdk-js/dist/legacy/index.js",
-  },
-};
