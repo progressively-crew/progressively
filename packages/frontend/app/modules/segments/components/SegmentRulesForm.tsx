aac936566271aed84b9109d2914091be57595407
Marvin Frachet <marvin.frachet@protonmail.com>
2023-04-02 16:44:54 +0200

    feat(segment): edit rule (#496)

--- packages/frontend/app/modules/segments/components/SegmentRulesForm.tsx
+++ packages/frontend/app/modules/segments/components/SegmentRulesForm.tsx
@@ -10,6 +10,7 @@ export const SegmentRulesForm = ({ rules }: SegmentRulesFormProps) => {
     <div className="flex flex-col gap-1">
       {rules.map((rule) => (
         <div className="bg-gray-50 dark:bg-slate-900 px-6 py-4" key={rule.uuid}>
+          <input type="hidden" name="uuid" value={rule.uuid} />
           <RuleFormField
             initialFieldName={rule.fieldName}
             initialFieldComparator={rule.fieldComparator}
