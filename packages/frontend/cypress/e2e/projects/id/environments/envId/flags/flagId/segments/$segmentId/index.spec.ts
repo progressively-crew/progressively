d4cbaee8afc29f8b326e24342ee774bc6d89d68b
mfrachet <undefined>
2023-04-02 09:21:11 +0200

    feat(segment): edit rule

--- packages/frontend/cypress/e2e/projects/id/environments/envId/flags/flagId/segments/$segmentId/index.spec.ts
+++ packages/frontend/cypress/e2e/projects/id/environments/envId/flags/flagId/segments/$segmentId/index.spec.ts
@@ -93,6 +93,63 @@ describe("/dashboard/projects/[id]/environments/[envId]/flags/[flagId]/segments/
           }).should("be.visible");
         });
       });
+
+      describe("Segment Update (rules)", () => {
+        beforeEach(() => {
+          cy.visit("/dashboard/projects/1/environments/1/flags/1/segments/1");
+          cy.injectAxe();
+        });
+
+        it("shows an initial form", () => {
+          cy.findByLabelText("Field name")
+            .should("be.visible")
+            .and("have.value", "email");
+
+          cy.findByLabelText("Field comparator")
+            .should("be.visible")
+            .and("have.text", "is in array");
+
+          cy.get("[name='field-value']").and("have.value", "gmail.com");
+
+          cy.checkA11y();
+        });
+
+        it("adds a new rule when pressing the button", () => {
+          cy.findAllByLabelText("Field name").should("have.length", 1);
+
+          cy.findByRole("button", { name: "Add a new rule" })
+            .should("be.visible")
+            .click();
+
+          cy.get("[name='field-name']").should("have.length", 2);
+          cy.checkA11y();
+
+          cy.reload();
+
+          cy.get("[name='field-name']").should("have.length", 2);
+        });
+
+        it("edits the existing rules", () => {
+          cy.findByRole("button", { name: "Add a new rule" }).click();
+          cy.get("[name='field-name']").should("have.length", 2);
+
+          cy.get("[name='field-name']").last().type("firstname");
+          cy.get("[name='tag-name']").last().type("hello world{enter}");
+          cy.findByRole("button", { name: "Save the rules" }).click();
+
+          cy.get("[name='field-name']").should("have.length", 2);
+
+          cy.get("[name='field-name']")
+            .last()
+            .should("have.attr", "value", "firstname");
+          cy.get("[name='field-value']")
+            .last()
+            .should("have.attr", "value", "hello world");
+          cy.findByText(
+            "The segment rules have been successfully edited."
+          ).should("be.visible");
+        });
+      });
     });
   });
 });
