90a0880dd8b95832b58ee75ab2459516a4af7176
mfrachet <undefined>
2023-04-30 09:16:46 +0200

    wip

--- packages/documentation/astro.config.mjs
+++ packages/documentation/astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
