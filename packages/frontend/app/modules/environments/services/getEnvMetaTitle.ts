e5d5c34220f7f461b9fc82ba2d71aee0be05babf
mfrachet <undefined>
2023-05-02 08:09:51 +0200

    chore(remix): routes v2

--- packages/frontend/app/modules/environments/services/getEnvMetaTitle.ts
+++ packages/frontend/app/modules/environments/services/getEnvMetaTitle.ts
@@ -2,7 +2,7 @@ import { Project } from "~/modules/projects/types";
 
 export const getEnvMetaTitle = (matches: any, envId: string): string => {
   const project: Project | undefined = matches.find(
-    (match: any) => match.id === "routes/dashboard/projects/$id"
+    (match: any) => match.id === "routes/dashboard.projects.$id"
   )?.data?.project;
 
   const environment = project?.environments.find((env) => env.uuid === envId);
--- packages/frontend/app/modules/projects/services/getProjectMetaTitle.ts
+++ packages/frontend/app/modules/projects/services/getProjectMetaTitle.ts
@@ -2,7 +2,7 @@ import { Project } from "../types";
 
 export const getProjectMetaTitle = (matches: any): string => {
   const project: Project | undefined = matches.find(
-    (match: any) => match.id === "routes/dashboard/projects/$id"
+    (match: any) => match.id === "routes/dashboard.projects.$id"
   )?.data?.project;
 
   return project?.name || "";
