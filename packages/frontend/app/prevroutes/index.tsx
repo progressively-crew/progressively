70c1d6c95dfce5ff1fc18248c6fd1b1747e1a283
mfrachet <undefined>
2023-05-02 07:53:00 +0200

    chore(remix): routes v2

--- packages/frontend/app/prevroutes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { LoaderFunction, redirect } from "@remix-run/node";
-
-export const loader: LoaderFunction = async () => {
-  return redirect("/signin");
-};
-
-export default function Homepage() {
-  return null;
-}
--- packages/frontend/remix.config.js
+++ packages/frontend/remix.config.js
@@ -21,6 +21,7 @@ module.exports = {
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
   },
   ...vercelSpecificConfig,
 };
