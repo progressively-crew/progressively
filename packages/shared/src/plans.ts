af64c0ed5074add600f60fc36eddf231b0ec1e20
Marvin Frachet <marvin.frachet@protonmail.com>
2023-05-07 13:32:07 +0200

    feat(pricing): simpler and better (#549)

--- packages/shared/src/plans.ts
+++
@@ -1,9 +0,0 @@
-export const Plans = {
-  OpenSource: "OpenSource",
-  Trialing: "Trialing",
-  Standard: "Standard",
-  Pro: "Pro",
-};
-
-export type PlansType = typeof Plans;
-export type PlanType = keyof typeof Plans;
