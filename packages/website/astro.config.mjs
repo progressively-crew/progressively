d5b8a8f14dcb137e835d0c260104703af14ee46a
mfrachet <undefined>
2023-05-07 13:10:44 +0200

    feat(pricing): simpler and better

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/edge";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: vercel(),
-  server: { port: 3001 },
+  server: {
+    port: 3001
+  }
 });
