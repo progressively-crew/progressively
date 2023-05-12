6b8ae77c6d1d14016d178879069a8b6fd9f236f8
mfrachet <undefined>
2023-05-12 10:54:09 +0200

    iterate

--- packages/frontend/app/modules/plans/hooks/useIsAuthorized.ts
+++
@@ -1,9 +0,0 @@
-import { useIsSaas } from "~/modules/saas/contexts/useIsSaas";
-
-export const useIsAuthorized = () => {
-  const isSaas = useIsSaas();
-
-  if (!isSaas) {
-    return true;
-  }
-};
---
+++ packages/frontend/app/modules/plans/hooks/useIsAuthorizedOnProject.ts
@@ -0,0 +1,21 @@
+import { useProjects } from "~/modules/projects/contexts/useProjects";
+import { useIsSaas } from "~/modules/saas/contexts/useIsSaas";
+import { useBillingInfo } from "./useBillingInfo";
+
+export const useIsAuthorizedOnProject = () => {
+  const isSaas = useIsSaas();
+  const { projects } = useProjects();
+  const billingInfo = useBillingInfo();
+
+  // Self hosted, do whatever you want
+  if (!isSaas) {
+    return true;
+  }
+
+  // SaaS with a paying subscriber
+  if (billingInfo.activePlan) {
+    return billingInfo.activePlan.projectCount > projects.length;
+  }
+
+  return false;
+};
