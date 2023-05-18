611c48a0285f6fd986b73d8221d0014bd8a9d24f
mfrachet <undefined>
2023-05-18 17:28:05 +0200

    fix(email): email should look better

--- packages/backend/src/mail/components/PrimaryButton.tsx
+++ packages/backend/src/mail/components/PrimaryButton.tsx
@@ -3,7 +3,7 @@ import { Button, ButtonProps } from '@react-email/components';
 export const PrimaryButton = (props: ButtonProps) => {
   return (
     <Button
-      className="h-10 px-4 whitespace-nowrap inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 cursor-pointer bg-gray-800 text-white hover:bg-gray-500"
+      className="py-4 px-4 whitespace-nowrap inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 cursor-pointer bg-gray-800 text-white hover:bg-gray-500"
       {...props}
     />
   );
