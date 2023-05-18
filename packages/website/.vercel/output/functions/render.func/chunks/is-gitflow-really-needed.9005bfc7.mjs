ad609b6fb8a950c419f74aca961a32c55609b3da
mfrachet <undefined>
2023-05-18 07:38:26 +0200

    feat(email): better looking emails

--- packages/website/.vercel/output/functions/render.func/chunks/is-gitflow-really-needed.9005bfc7.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./is-gitflow-really-needed.c02c254a.mjs');
-					}
-					const collectedLinks = "@@ASTRO-LINKS@@";
-					const collectedStyles = "@@ASTRO-STYLES@@";
-					const collectedScripts = "@@ASTRO-SCRIPTS@@";
-
-export { collectedLinks, collectedScripts, collectedStyles, getMod };
