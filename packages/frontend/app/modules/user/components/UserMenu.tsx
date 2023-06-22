88e9ab4d639f9da48bb3f5560cb7edfb0ac3541c
mfrachet <undefined>
2023-06-22 13:15:35 +0200

    refactor(flag): remove old flag

--- packages/frontend/app/modules/user/components/UserMenu.tsx
+++ packages/frontend/app/modules/user/components/UserMenu.tsx
@@ -1,4 +1,3 @@
-import { useFlags } from "@progressively/react";
 import { HorizontalNav, NavItem } from "~/components/HorizontalNav";
 import { useIsSaas } from "~/modules/saas/contexts/useIsSaas";
 
@@ -9,16 +8,13 @@ export interface FlagMenuProps {
 }
 
 export const UserMenu = () => {
-  const { flags } = useFlags();
   const isSaas = useIsSaas();
 
   return (
     <HorizontalNav label={`Flag related`}>
       <NavItem to="/dashboard/profile">General</NavItem>
 
-      {flags.pricingPage && isSaas && (
-        <NavItem to="/dashboard/profile/billing">Billing</NavItem>
-      )}
+      {isSaas && <NavItem to="/dashboard/profile/billing">Billing</NavItem>}
     </HorizontalNav>
   );
 };
