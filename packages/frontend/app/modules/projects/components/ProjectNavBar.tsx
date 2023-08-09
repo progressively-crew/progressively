d730f9acfe3f5e1b35d639f4adcab3aab9d984f7
Marvin Frachet <marvin.frachet@protonmail.com>
2023-08-09 09:56:12 +0200

    ux(project): better submenu (#645)

--- packages/frontend/app/modules/projects/components/ProjectNavBar.tsx
+++ packages/frontend/app/modules/projects/components/ProjectNavBar.tsx
@@ -3,14 +3,25 @@ import { Project } from "../types";
 import { EnvIcon } from "~/components/Icons/EnvIcon";
 import { FlagIcon } from "~/components/Icons/FlagIcon";
 import { SettingsIcon } from "~/components/Icons/SettingsIcon";
-import { BsArrow90DegDown } from "react-icons/bs";
+import { BsCaretRightFill } from "react-icons/bs";
 import { AiOutlinePlus } from "react-icons/ai";
+import { useState } from "react";
+import { IconBox } from "~/components/IconBox";
+import { useLocation } from "@remix-run/react";
 
 export interface ProjectNavBarProps {
   project: Project;
 }
 
 export const ProjectNavBar = ({ project }: ProjectNavBarProps) => {
+  const location = useLocation();
+  const [expanded, setExpanded] = useState(
+    location.pathname.includes("/environments")
+  );
+
+  const buttonStyles =
+    "gap-2 w-full focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:focus:ring-offset-slate-900 text-sm h-8 block flex items-center rounded px-3 text-gray-700 hover:bg-gray-50 hover:dark:bg-slate-700 dark:text-gray-300";
+
   return (
     <HorizontalNav label={"Navigate in project"}>
       <NavItem
@@ -21,16 +32,29 @@ export const ProjectNavBar = ({ project }: ProjectNavBarProps) => {
       </NavItem>
 
       <li className="text-sm block rounded text-gray-900 dark:text-white px-4">
-        <div className="h-8 flex items-center gap-2 px-3 text-gray-700 pb-1 dark:text-slate-50">
-          <EnvIcon />
+        <button className={buttonStyles} onClick={() => setExpanded((s) => !s)}>
+          <BsCaretRightFill
+            className={expanded ? "rotate-90 transition-all" : "transition-all"}
+          />
           Environments
-        </div>
-        <ul className="flex flex-col gap-2 pl-2">
+        </button>
+        <ul
+          className={`pt-1 flex flex-col gap-2 pl-2 ${
+            expanded ? "block" : "hidden"
+          }`}
+          aria-hidden={!expanded}
+          aria-expanded={expanded}
+          aria-label="Environments submenu"
+        >
           {project.environments.map((env) => (
             <NavItem
               to={`/dashboard/projects/${project.uuid}/environments/${env.uuid}`}
               key={env.uuid}
-              icon={<BsArrow90DegDown className="-rotate-90" />}
+              icon={
+                <IconBox content={env.name} size="S">
+                  <EnvIcon />
+                </IconBox>
+              }
             >
               {env.name}
             </NavItem>
