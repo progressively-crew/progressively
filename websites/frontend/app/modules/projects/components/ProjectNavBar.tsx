3797ff7f255411f299ef8fab576388076696e978
mfrachet <undefined>
2023-11-28 09:52:02 +0100

    wip

--- websites/frontend/app/modules/projects/components/ProjectNavBar.tsx
+++ websites/frontend/app/modules/projects/components/ProjectNavBar.tsx
@@ -2,8 +2,6 @@ import { HorizontalNav, NavItem } from "~/components/HorizontalNav";
 import { Project } from "../types";
 import { FlagIcon } from "~/components/Icons/FlagIcon";
 import { SettingsIcon } from "~/components/Icons/SettingsIcon";
-
-import { EnvMenuButton } from "~/modules/environments/components/EnvMenuButton";
 import { TbChartAreaLine } from "react-icons/tb";
 
 export interface ProjectNavBarProps {
@@ -30,13 +28,6 @@ export const ProjectNavBar = ({ project }: ProjectNavBarProps) => {
           Insights
         </NavItem>
       )}
-
-      <li className="text-sm block rounded text-gray-900 dark:text-white">
-        <EnvMenuButton
-          projectId={project.uuid}
-          environments={project.environments}
-        />
-      </li>
 
       <NavItem
         to={`/dashboard/projects/${project.uuid}/settings`}
