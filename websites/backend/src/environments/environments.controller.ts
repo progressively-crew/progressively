58bfa4ca037b2382006e589ebf839e94cc58c64e
mfrachet <undefined>
2023-12-14 09:43:22 +0100

    fix(analytics): bounce rate problem cause of bigint

--- websites/backend/src/environments/environments.controller.ts
+++ websites/backend/src/environments/environments.controller.ts
@@ -52,6 +52,34 @@ export class EnvironmentsController {
     return this.envService.rotateSecretKey(envId);
   }
 
+  @Get(':envId/metrics/count')
+  @UseGuards(HasEnvironmentAccessGuard)
+  @UseGuards(JwtAuthGuard)
+  async getMetricCount(
+    @Param('envId') envId: string,
+    @Query('startDate') startDate: string | undefined,
+    @Query('endDate') endDate: string | undefined,
+  ) {
+    if (!endDate || !startDate) {
+      throw new BadRequestException('startDate and endDate are required.');
+    }
+
+    const metricCount = await this.envService.getMetricCount(
+      envId,
+      startDate,
+      endDate,
+    );
+
+    const pageViewCount = await this.envService.getMetricCount(
+      envId,
+      startDate,
+      endDate,
+      'Page View',
+    );
+
+    return { metricCount, pageViewCount };
+  }
+
   @Get(':envId/events')
   @UseGuards(HasEnvironmentAccessGuard)
   @UseGuards(JwtAuthGuard)
@@ -108,19 +136,6 @@ export class EnvironmentsController {
         'referer',
       );
 
-    const metricCount = await this.envService.getMetricCount(
-      envId,
-      startDate,
-      endDate,
-    );
-
-    const pageViewCount = await this.envService.getMetricCount(
-      envId,
-      startDate,
-      endDate,
-      'Page View',
-    );
-
     const uniqueVisitors = await this.envService.getUniqueVisitor(
       envId,
       startDate,
@@ -140,8 +155,6 @@ export class EnvironmentsController {
       eventsPerDatePerBrowser,
       eventsPerDatePerUrl,
       eventsPerDatePerReferer,
-      metricCount,
-      pageViewCount,
       uniqueVisitorsCount: uniqueVisitors.length,
       bounceRate,
     };
--- websites/frontend/app/components/BigStat.tsx
+++ websites/frontend/app/components/BigStat.tsx
@@ -1,16 +1,25 @@
 import { Card, CardContent } from "./Card";
 import { NumberValue } from "./NumberValue";
+import { Tag } from "./Tag";
 import { Typography } from "./Typography";
 
 export interface BigStatProps {
   label: string;
   value: number;
-  unit: string;
+  unit?: string;
   icon: React.ReactNode;
   detail?: React.ReactNode;
+  evolution?: number;
 }
 
-export const BigStat = ({ label, value, unit, icon, detail }: BigStatProps) => {
+export const BigStat = ({
+  label,
+  value,
+  unit,
+  icon,
+  detail,
+  evolution,
+}: BigStatProps) => {
   return (
     <Card>
       <CardContent>
@@ -24,8 +33,26 @@ export const BigStat = ({ label, value, unit, icon, detail }: BigStatProps) => {
               <strong>
                 <NumberValue value={value} />
               </strong>{" "}
-              <span className="text-slate-500 dark:text-slate-300 text-xl">
-                {unit}
+              <span className="inline-flex flex-row items-center">
+                <span className="text-slate-500 dark:text-slate-300 text-xl">
+                  {unit}
+                </span>
+                {evolution && evolution !== 0 ? (
+                  <span className="text-xs ml-2">
+                    <Tag
+                      size="S"
+                      variant={
+                        evolution > 0
+                          ? "SUCCESS"
+                          : evolution < 0
+                          ? "DANGER"
+                          : "DEFAULT"
+                      }
+                    >
+                      {evolution}%
+                    </Tag>
+                  </span>
+                ) : null}
               </span>
             </Typography>
 
