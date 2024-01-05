14cc85b96fd8a6ec931a377f8de926e02411f9ab
mfrachet <undefined>
2024-01-05 10:18:06 +0100

    website(marketing): marketing website runtime ssr

--- websites/marketing/astro.config.mjs
+++ websites/marketing/astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: netlify(),
 });
