815f1ecbe539b86ecc2571f5a6592cb74f0b598d
mfrachet <undefined>
2024-01-12 15:29:49 +0100

    ui(toast): better toast

--- websites/frontend/app/components/Boxes/SuccessBox.tsx
+++ websites/frontend/app/components/Boxes/SuccessBox.tsx
@@ -1,8 +1,7 @@
 import { Link, useLocation } from "@remix-run/react";
 import { useEffect, useRef, useState } from "react";
-import { AiOutlineCheckCircle } from "react-icons/ai";
 import { MdClose } from "react-icons/md";
-import { HStack } from "../HStack";
+import { Background } from "../Background";
 
 export interface SuccessBoxProps {
   children: React.ReactNode;
@@ -33,32 +32,34 @@ export const SuccessBox = ({ children, id, ...props }: SuccessBoxProps) => {
   if (!isVisible) return null;
 
   return (
-    <div
-      className="z-20 bg-white dark:bg-white shadow-lg p-4 bg-white border border-slate-200 rounded motion-safe:animate-fade-enter-bottom fixed right-4 bottom-4"
-      key={id}
-    >
-      <div className="gap-2 max-w-5xl w-full mx-auto flex flex-row justify-between items-center">
-        <p
-          ref={boxRef}
-          tabIndex={-1}
-          id={id}
-          {...props}
-          className="success-box"
-        >
-          <HStack as="span" spacing={2}>
-            <AiOutlineCheckCircle aria-hidden className="text-emerald-400" />
-            <span className="text-sm">{children}</span>
-          </HStack>
-        </p>
+    <div className="motion-safe:animate-fade-enter-bottom fixed right-8 bottom-8 z-10 rounded-lg overflow-hidden shadow-xl">
+      <Background spacing="S">
+        <div className="rounded bg-white dark:bg-slate-900 p-4 flex flex-row gap-4">
+          <div>
+            <p className="font-bold text-sm dark:text-white">
+              🚀 Operation succeeded!
+            </p>
 
-        <Link
-          to={location.pathname}
-          className="text-xl rounded bg-transparent hover:bg-slate-100 active:bg-slate-200 flex items-center justify-center w-6 h-6"
-          preventScrollReset={true}
-        >
-          <MdClose aria-label="Close the banner" />
-        </Link>
-      </div>
+            <p
+              ref={boxRef}
+              tabIndex={-1}
+              id={id}
+              {...props}
+              className="success-box text-sm dark:text-white"
+            >
+              {children}
+            </p>
+          </div>
+
+          <Link
+            to={location.pathname}
+            className="text-xl rounded bg-transparent hover:bg-slate-100 active:bg-slate-200 flex items-center justify-center w-6 h-6"
+            preventScrollReset={true}
+          >
+            <MdClose aria-label="Close the banner" />
+          </Link>
+        </div>
+      </Background>
     </div>
   );
 };
