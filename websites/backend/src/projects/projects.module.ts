c00be6bfff14ad0490898e9fb34653988cede7a7
mfrachet <marvin.frachet@gmail.com>
2024-02-08 09:29:36 +0100

    huge iteration

--- websites/backend/src/projects/projects.module.ts
+++ websites/backend/src/projects/projects.module.ts
@@ -4,9 +4,10 @@ import { ProjectsService } from './projects.service';
 import { UsersModule } from '../users/users.module';
 import { MailModule } from '../mail/mail.module';
 import { DatabaseModule } from '../database/database.module';
+import { FunnelsModule } from '../funnels/funnels.module';
 
 @Module({
-  imports: [UsersModule, MailModule, DatabaseModule],
+  imports: [UsersModule, MailModule, DatabaseModule, FunnelsModule],
   controllers: [ProjectsController],
   providers: [ProjectsService],
   exports: [ProjectsService],
