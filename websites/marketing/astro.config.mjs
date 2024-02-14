1ee0486cbd058a5f242f309e87a4c82ac1ae1569
mfrachet <marvin.frachet@gmail.com>
2024-02-14 11:16:43 +0100

    feat(deploy): marketing website to vercel

--- websites/marketing/astro.config.mjs
+++ websites/marketing/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
 });
