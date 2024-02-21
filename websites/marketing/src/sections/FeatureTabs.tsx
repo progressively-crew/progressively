042595219940327cc93da6a584e00cb4f2f9aae0
mfrachet <marvin.frachet@gmail.com>
2024-02-21 15:13:43 +0100

    refactor(marketing): better marketing page

--- websites/marketing/src/sections/FeatureTabs.tsx
+++ websites/marketing/src/sections/FeatureTabs.tsx
@@ -1,4 +1,14 @@
 import { Tabs } from "@radix-ui/react-tabs";
+import { LuAlarmClock } from "react-icons/lu";
+import { MdOutlineAttractions } from "react-icons/md";
+import { IoMdLink } from "react-icons/io";
+import { SiWindows95 } from "react-icons/si";
+import { CgBrowser } from "react-icons/cg";
+import { BiStats } from "react-icons/bi";
+import { FaUserShield } from "react-icons/fa6";
+import { IoMdApps } from "react-icons/io";
+import { AiOutlineLineChart } from "react-icons/ai";
+import { MdSmartphone } from "react-icons/md";
 import { Tab, TabList, TabPanel } from "../components/Tabs";
 import { Browser } from "../components/Browser";
 import ffSrc from "./assets/above-fold/ff.png";
@@ -9,8 +19,19 @@ export interface FeatureTabsProps {
   codes: Array<string>;
 }
 
+const Box = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-8 w-8 rounded-lg bg-slate-200 flex flex-row items-center justify-center">
+      {children}
+    </div>
+  );
+};
+
 export const FeatureTabs = ({ codes }: FeatureTabsProps) => {
   const titleClass = "font-bold text-2xl";
+  const textClass = "text-slate-600";
+
+  const liClass = `${textClass} flex flex-row items-center gap-2`;
 
   return (
     <Tabs defaultValue="first">
@@ -27,27 +48,107 @@ export const FeatureTabs = ({ codes }: FeatureTabsProps) => {
       </TabList>
 
       <TabPanel value="first">
-        <Browser>
-          <div className="p-2">Blahblah</div>
-        </Browser>
-        <Code html={codes[0]} />
+        <div className="grid grid-cols-2 gap-8 items-center">
+          <div className="flex flex-col gap-4">
+            <h3 className={titleClass}>Feature flags</h3>
+
+            <p className={textClass}>
+              Feature flags let you turn features on or off with just a click.
+              This means you can release new features whenever you're ready.
+              Progressively unlocks even more features:
+            </p>
+
+            <ul className="flex flex-col gap-2">
+              <li className={liClass}>
+                <Box>
+                  <IoMdApps />
+                </Box>
+                Single & multi variants
+              </li>
+              <li className={liClass}>
+                <Box>
+                  <LuAlarmClock />
+                </Box>
+                Schedule rollout
+              </li>
+              <li className={liClass}>
+                <Box>
+                  <AiOutlineLineChart />
+                </Box>
+                Gradual rollout
+              </li>
+              <li className={liClass}>
+                <Box>
+                  <MdOutlineAttractions />
+                </Box>
+                Attribute based targeting
+              </li>
+              <li className={liClass}>
+                <Box>
+                  <FaUserShield />
+                </Box>
+                Anonymous
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <Browser>
+              <div className="p-2">
+                <img src={ffSrc.src} alt="Woop" />
+              </div>
+            </Browser>
+          </div>
+        </div>
       </TabPanel>
       <TabPanel value="second">
-        <div className="grid grid-cols-[auto_1fr] gap-8">
-          <div className="flex flex-col gap-2">
-            <h3 className={titleClass}>As simple as adding a script tag</h3>
+        <div className="grid grid-cols-2 gap-8 items-center">
+          <div className="flex flex-col gap-4">
+            <h3 className={titleClass}>Quantitative analytics</h3>
 
-            <ol className="list-decimal pl-5 flex flex-col gap-4">
-              <li>
-                <p className="pb-2 text-slate-700">
-                  Add this one on your website
-                </p>
-                <Code html={codes[1]} />
+            <p className={textClass}>
+              Collect details about the page being looked at, and include extra
+              activities to learn more about your audience's actions.
+            </p>
+
+            <ul className="flex flex-col gap-2">
+              <li className={liClass}>
+                <Box>
+                  <CgBrowser />
+                </Box>
+                Page views by browser
               </li>
-              <li>
-                <p className="pb-2 text-slate-700">Enjoy.</p>
+              <li className={liClass}>
+                <Box>
+                  <SiWindows95 />
+                </Box>
+                Page views by OS
               </li>
-            </ol>
+              <li className={liClass}>
+                <Box>
+                  <MdSmartphone />
+                </Box>
+                Page views by viewport
+              </li>
+              <li className={liClass}>
+                <Box>
+                  <IoMdLink />
+                </Box>
+                Page views by referer
+              </li>
+              <li className={liClass}>
+                <Box>
+                  <BiStats />
+                </Box>
+                Unique visitors & bounce rate
+              </li>
+              <li className={liClass}>
+                <Box>
+                  <FaUserShield />
+                </Box>
+                Anonymous
+              </li>
+            </ul>
           </div>
 
           <Browser>
