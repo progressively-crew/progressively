816387a0056446062392c97b8c9c8d9ad67ca6e8
Marvin Frachet <marvin.frachet@protonmail.com>
2024-05-21 16:20:30 +0200

    fix(marketing): deploy marketing again

--- websites/marketing/astro.config.mjs
+++ websites/marketing/astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(),
 });
