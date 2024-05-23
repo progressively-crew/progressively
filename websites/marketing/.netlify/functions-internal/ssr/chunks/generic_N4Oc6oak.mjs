1f34485de839a77c38bfeca48d04986cc745dfc2
Marvin Frachet <marvin.frachet@protonmail.com>
2024-05-23 16:44:15 +0200

    fix(backend): websocket issue and banner not refreshing (#800)

--- websites/marketing/.netlify/functions-internal/ssr/chunks/generic_N4Oc6oak.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_4iGCxdVM.mjs').then(n => n.g);
-
-export { page };
--- websites/marketing/.netlify/functions-internal/ssr/chunks/index_acExwZyh.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/index_uIOgrx5L.mjs');
-
-export { page };
