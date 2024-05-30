d8c49f7da544c471c43eb0cf4693ddbd9555389c
Marvin Frachet <marvin.frachet@protonmail.com>
2024-05-30 15:40:24 +0200

    refacto(frontend): move to remix + vite

--- websites/frontend/vite.config.ts
+++ websites/frontend/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     remix({
       future: {
