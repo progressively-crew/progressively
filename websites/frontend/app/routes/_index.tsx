2470c7afd729e2698ca0a7aa52a5ed13c8e861b0
Marvin Frachet <marvin.frachet@protonmail.com>
2024-05-30 14:16:19 +0200

    feat(frontend): redirect when already logged in

--- websites/frontend/app/routes/_index.tsx
+++ websites/frontend/app/routes/_index.tsx
@@ -1,7 +1,15 @@
 import { LoaderFunction, redirect } from "@remix-run/node";
+import { getSession } from "~/sessions";
 
-export const loader: LoaderFunction = async () => {
-  return redirect("/signin");
+export const loader: LoaderFunction = async ({ request }) => {
+  const session = await getSession(request.headers.get("Cookie"));
+  const authCookie = session.get("auth-cookie");
+
+  if (!authCookie) {
+    throw redirect("/signin");
+  }
+
+  return redirect("/dashboard/projects/all");
 };
 
 export default function Homepage() {
