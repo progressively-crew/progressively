7b3afb6d2a1443cb3626ed9fa6ad983ada2783f8
Marvin Frachet <marvin.frachet@protonmail.com>
2024-06-18 15:26:42 +0200

    wip

--- websites/documentation/astro.config.mjs
+++ websites/documentation/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from "@astrojs/starlight";
 export default defineConfig({
   integrations: [
     starlight({
-      title: "My Docs",
+      title: "Progressively",
       social: {
         github: "https://github.com/withastro/starlight",
       },
