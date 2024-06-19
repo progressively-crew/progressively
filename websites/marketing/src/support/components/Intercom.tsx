d0f863b04e0f67a1890548d0018bab6facfaff4c
Marvin Frachet <marvin.frachet@protonmail.com>
2024-06-19 15:31:51 +0200

    feat(support): add intercom

--- websites/marketing/src/support/components/Intercom.tsx
+++ websites/marketing/src/support/components/Intercom.tsx
@@ -3,5 +3,6 @@ import { IntercomProvider } from "react-use-intercom";
 const INTERCOM_APP_ID = "lnfovtpj";
 
 export const Intercom = () => {
+  // @ts-ignore
   return <IntercomProvider appId={INTERCOM_APP_ID} autoBoot></IntercomProvider>;
 };
