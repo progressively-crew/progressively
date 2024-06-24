d062fcadd56aa689c6566abc3158a17358b4b2f4
Marvin Frachet <marvin.frachet@protonmail.com>
2024-06-24 16:33:40 +0200

    feat(flags): add segment resolution

--- packages/database/prisma/migrations/20240624143331_add_segment_to_rule/migration.sql
+++ packages/database/prisma/migrations/20240624143331_add_segment_to_rule/migration.sql
@@ -1,8 +1,5 @@
--- DropForeignKey
-ALTER TABLE "Rule" DROP CONSTRAINT "Rule_segmentUuid_fkey";
-
 -- AlterTable
-ALTER TABLE "Rule" ALTER COLUMN "segmentUuid" DROP NOT NULL;
+ALTER TABLE "Rule" ADD COLUMN     "segmentUuid" TEXT;
 
 -- AddForeignKey
 ALTER TABLE "Rule" ADD CONSTRAINT "Rule_segmentUuid_fkey" FOREIGN KEY ("segmentUuid") REFERENCES "Segment"("uuid") ON DELETE SET NULL ON UPDATE CASCADE;
