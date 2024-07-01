2428ca9331d2e09106803a0130d7bd1c2a3b5ab9
Marvin Frachet <marvin.frachet@protonmail.com>
2024-07-01 20:56:41 +0200

    feat(frontend): add icons

--- websites/frontend/app/modules/analytics/helpers/getOSIcon.tsx
+++ websites/frontend/app/modules/analytics/helpers/getOSIcon.tsx
@@ -1,11 +1,13 @@
-import { FaApple, FaLinux, FaWindows } from "react-icons/fa";
-import { MdComputer } from "react-icons/md";
+import { FaAndroid, FaApple, FaLinux, FaWindows } from "react-icons/fa";
+import { MdComputer, MdPhoneIphone } from "react-icons/md";
 
 const iconClass = "relative z-10";
 
 export const getOSIcon = (osName: string) => {
   const os = osName.toLowerCase();
 
+  if (os.includes("ios")) return <MdPhoneIphone className={iconClass} />;
+  if (os.includes("android")) return <FaAndroid className={iconClass} />;
   if (os.includes("windows")) return <FaWindows className={iconClass} />;
   if (os.includes("mac")) return <FaApple className={iconClass} />;
   if (os.includes("linux")) return <FaLinux className={iconClass} />;
